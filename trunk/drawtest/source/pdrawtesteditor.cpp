7ee846c31a879b679fa86e739f2200d744f9fa0b
scheffle <scheffle@da8dfa92-c2a0-46b0-bef3-9efb2d1af313>
2004-12-05 12:34:35 +0000

    new draw test plug code.

--- trunk/drawtest/source/pdrawtesteditor.cpp
+++ trunk/drawtest/source/pdrawtesteditor.cpp
@@ -19,19 +19,116 @@
 
 #include "pdrawtesteditor.h"
 #include "pdrawtestview.h"
+#include "cscrollview.h"
+#include "ctabview.h"
+#include "controlsgui.h"
+
+enum {
+	kBackgroundBitmap	= 1,
+	kTabButtonBitmap	= 100,
+	kTestBitmap			= 1000,
+};
+
+class MyTabView : public CTabView
+{
+public:
+	MyTabView (const CRect& size, CFrame* parent, CBitmap* tabBitmap, CBitmap* background = 0, long tabPosition = kPositionTop, DrawTestEditor* editor = 0)
+	: CTabView (size, parent, tabBitmap, background, tabPosition)
+	, editor (editor) {}
+
+	virtual void mouse (CDrawContext *pContext, CPoint &where, long button = -1)
+	{
+		if (button == kRButton)
+		{
+			CView* view = getViewAt (where);
+			if (!view || view->isTypeOf ("CTabButton"))
+			{
+				CRect r;
+				localToFrame (where);
+				r.offset (where.x, where.y);
+				r.offset (-size.left, -size.top);
+				COptionMenu* menu = new COptionMenu (r, NULL, 0);
+				menu->addEntry ("Tabs Left");
+				menu->addEntry ("Tabs Right");
+				menu->addEntry ("Tabs Top");
+				menu->addEntry ("Tabs Bottom");
+				getFrame ()->addView (menu);
+				menu->takeFocus ();
+				long res = menu->getLastResult ();
+				getFrame ()->removeView (menu);
+				if (res != -1)
+				{
+					r = size;
+					editor->setTabView (getFrame (), r, res);
+				}
+				return;
+			}
+		}
+		CTabView::mouse (pContext, where, button);
+	}
+
+protected:
+	DrawTestEditor* editor;
+};
 
 DrawTestEditor::DrawTestEditor (AudioEffectX* effect)
 : AEffGUIEditor (effect)
 {
+	backgroundBitmap = new CBitmap (kBackgroundBitmap);
 	// setup size of editor
 	rect.left   = 0;
 	rect.top    = 0;
-	rect.right  = 400;
-	rect.bottom = 400;
+	rect.right  = backgroundBitmap->getWidth ();
+	rect.bottom = backgroundBitmap->getHeight ();
 }
 
 DrawTestEditor::~DrawTestEditor ()
 {
+	backgroundBitmap->forget ();
+}
+
+void DrawTestEditor::valueChanged (CDrawContext *pContext, CControl *pControl)
+{
+}
+
+void DrawTestEditor::setTabView (CFrame* frame, const CRect& r, long position)
+{
+	frame->removeAll ();
+	CBitmap* tabButtonBitmap = new CBitmap (kTabButtonBitmap);
+	CTabView* tabView = new MyTabView (r, frame, tabButtonBitmap, NULL, position, this);
+	tabView->setTransparency (true);
+	frame->addView (tabView);
+	CRect tabSize = tabView->getTabViewSize (tabSize);
+	// add tabs
+	CView* testView;
+	CBitmap* testBitmap = new CBitmap (kTestBitmap);
+	CRect containerSize;
+	containerSize.right = testBitmap->getWidth ();
+	containerSize.bottom = testBitmap->getHeight ();
+	CScrollView* scrollview = new CScrollView (tabSize, containerSize, frame, CScrollView::kHorizontalScrollbar|CScrollView::kVerticalScrollbar);
+	CPoint p (0,0);
+	testView = new CMovieBitmap (containerSize, NULL, 0, 1, testBitmap->getHeight (), testBitmap, p);
+	testBitmap->forget ();
+	scrollview->addView (testView);
+	tabView->addTab (scrollview, "Scroll View");
+
+	testView = new CDrawTestView (tabSize);
+	tabView->addTab (testView, "Primitives");
+	
+	CRect controlsGUISize (0, 0, 420, 210);
+	controlsGUISize.offset (5, 5);
+	testView = new ControlsGUI (controlsGUISize, frame);
+
+	CViewContainer* controlContainer = new CViewContainer (tabSize, frame);
+	controlContainer->setTransparency (true);
+	controlContainer->setMode (CViewContainer::kOnlyDirtyUpdate);
+	controlContainer->addView (testView);
+	
+	tabView->addTab (controlContainer, "Controls");
+
+
+	tabButtonBitmap->forget ();
+	frame->setDirty ();
 }
 
 long DrawTestEditor::open (void *ptr)
@@ -39,11 +136,10 @@ long DrawTestEditor::open (void *ptr)
 	AEffGUIEditor::open (ptr);
 	CRect size (rect.left , rect.top, rect.right, rect.bottom);
 	CFrame* frame = new CFrame (size, ptr, this);
-
-	size.inset (5, 5);
-	CDrawTestView* view = new CDrawTestView (size);
-	frame->addView (view);
-
+	frame->setBackground (backgroundBitmap);
+	size.inset (8, 8);
+	setTabView (frame, size, CTabView::kPositionBottom);
+	// last but not least set the class variable frame to our newly created frame
 	this->frame = frame;
 	return true;
 }
