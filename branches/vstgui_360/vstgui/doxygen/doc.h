0254e29ba56214ac25e41b0abd1d36eb724e9e34
nobody <nobody@da8dfa92-c2a0-46b0-bef3-9efb2d1af313>
2009-07-17 12:06:24 +0000

    This commit was manufactured by cvs2svn to create branch 'vstgui_360'.

---
+++ branches/vstgui_360/vstgui/doxygen/doc.h
@@ -0,0 +1,64 @@
+//-----------------------------------------------------------------------------
+// VSTGUI: Graphical User Interface Framework not only for VST plugins
+//
+// Doxygen Documentation
+//
+// Version 3.6
+//
+//-----------------------------------------------------------------------------
+//
+//-----------------------------------------------------------------------------
+// VSTGUI LICENSE
+// (c) 2008, Steinberg Media Technologies, All Rights Reserved
+//-----------------------------------------------------------------------------
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+// 
+//   * Redistributions of source code must retain the above copyright notice, 
+//     this list of conditions and the following disclaimer.
+//   * Redistributions in binary form must reproduce the above copyright notice,
+//     this list of conditions and the following disclaimer in the documentation 
+//     and/or other materials provided with the distribution.
+//   * Neither the name of the Steinberg Media Technologies nor the names of its
+//     contributors may be used to endorse or promote products derived from this 
+//     software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A  PARTICULAR PURPOSE ARE DISCLAIMED. 
+// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
+// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE  OF THIS SOFTWARE, EVEN IF ADVISED
+// OF THE POSSIBILITY OF SUCH DAMAGE.
+//-----------------------------------------------------------------------------
+
+/**
+
+@mainpage
+
+Welcome to VSTGUI
+
+- @ref intro @n
+- @ref tutorial_getting_started @n
+- @ref changes_from_3_0 @n
+- @ref changelog @n
+- @ref license @n
+
+@section intro Introduction
+
+\par VSTGUI 
+VSTGUI is a User Interface Toolkit mainly for Audio Plug-Ins (VST, AudioUnit, etc).
+\par History
+First developed inhouse of Steinberg Media Technologies (around 1998) for their first VST Plug-Ins. 
+Later added as binary libraries to the official VST SDK. 
+Since May 2003 VSTGUI is open source and hosted at sourceforge.
+\par Platforms
+Currently VSTGUI compiles on Microsoft Windows 2000/ME/XP/XP64/Vista/Vista64 and Apple Mac OS X 10.4 - 10.5. 
+
+\sa
+<a href="http://vstgui.sf.net" target=_blank> VSTGUI @ Sourceforge </a>
+
+*/
