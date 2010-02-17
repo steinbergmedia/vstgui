9dd37ca0acf3595b3b9d6af6ec1a1720ed0e48a8
scheffle <scheffle@users.noreply.github.com>
2010-02-17 11:54:14 +0000

    add subcontroller test and win project

---
+++ vstgui/tests/uidescription vst3/source/factory.cpp
@@ -0,0 +1,86 @@
+/*
+ *  factory.cpp
+ *  uidescription test
+ *
+ *  Created by Arne Scheffler on 2/12/10.
+ *  Copyright 2010 Arne Scheffler. All rights reserved.
+ *
+ */
+
+#include "public.sdk/source/vst/vst2wrapper/vst2wrapper.h"
+#include "uidescription test.h"
+#include "subcontrollertest.h"
+#include "public.sdk/source/main/pluginfactoryvst3.h"
+
+//------------------------------------------------------------------------
+//------------------------------------------------------------------------
+//------------------------------------------------------------------------
+BEGIN_FACTORY("VSTGUI", "", "", PFactoryInfo::kUnicode)
+
+	DEF_CLASS2 (INLINE_UID_FROM_FUID(UIDescriptionTestProcessor::cid),
+				PClassInfo::kManyInstances,
+				kVstAudioEffectClass,
+				"VSTGUI UIDescription Test",
+				Vst::kDistributable,
+				"Fx",
+				"1.0.0",
+				kVstVersionString,
+				UIDescriptionTestProcessor::createInstance)
+				
+	DEF_CLASS2 (INLINE_UID_FROM_FUID(UIDescriptionTestController::cid),
+				PClassInfo::kManyInstances,
+				kVstComponentControllerClass,
+				"VSTGUI UIDescription Test",
+				Vst::kDistributable,
+				"Fx",
+				"1.0.0",
+				kVstVersionString,
+				UIDescriptionTestController::createInstance)
+				
+	DEF_CLASS2 (INLINE_UID_FROM_FUID(SubControllerTestProcessor::cid),
+				PClassInfo::kManyInstances,
+				kVstAudioEffectClass,
+				"VSTGUI UIDescription SubController Test",
+				Vst::kDistributable,
+				"Fx",
+				"1.0.0",
+				kVstVersionString,
+				SubControllerTestProcessor::createInstance)
+				
+	DEF_CLASS2 (INLINE_UID_FROM_FUID(SubControllerTestController::cid),
+				PClassInfo::kManyInstances,
+				kVstComponentControllerClass,
+				"VSTGUI UIDescription SubController Test",
+				Vst::kDistributable,
+				"Fx",
+				"1.0.0",
+				kVstVersionString,
+				SubControllerTestController::createInstance)
+				
+END_FACTORY
+
+//------------------------------------------------------------------------
+//------------------------------------------------------------------------
+//------------------------------------------------------------------------
+void* hInstance = 0;
+
+//------------------------------------------------------------------------
+bool InitModule ()   
+{
+	extern void* moduleHandle;
+	hInstance = moduleHandle;
+
+	return true; 
+}
+
+//------------------------------------------------------------------------
+bool DeinitModule ()
+{
+	return true; 
+}
+
+//------------------------------------------------------------------------
+::AudioEffect* createEffectInstance (::audioMasterCallback audioMaster)
+{
+	return Steinberg::Vst::Vst2Wrapper::create (GetPluginFactory (), UIDescriptionTestProcessor::cid, 0, audioMaster);
+}
--- vstgui/tests/uidescription vst3/source/uidescription test.cpp
+++ vstgui/tests/uidescription vst3/source/uidescription test.cpp
@@ -7,34 +7,39 @@
  *
  */
 
-#include "public.sdk/source/vst/vst2wrapper/vst2wrapper.h"
-#include "public.sdk/source/vst/vstaudioeffect.h"
-#include "public.sdk/source/main/pluginfactoryvst3.h"
+#include "uidescription test.h"
 #include "pluginterfaces/base/ibstream.h"
-#include "pluginterfaces/base/ustring.h"
 #include "pluginterfaces/vst/ivstparameterchanges.h"
-#include "vstgui/uidescription/vst3editor.h"
+#include <cmath>
 
 using namespace Steinberg;
+using namespace Steinberg::Vst;
 
 namespace VSTGUI {
 
+//------------------------------------------------------------------------
+FUID UIDescriptionTestProcessor::cid (0x49BAF003, 0xB44D455E, 0x9CBDE54F, 0x7FF2CBA1);
+FUID UIDescriptionTestController::cid (0xF0FF3C24, 0x3F2F4C94, 0x84F6B6AE, 0xEF7BF28B);
+
+//------------------------------------------------------------------------
 enum {
 	kPeakParam = 20,
 };
 
 //------------------------------------------------------------------------
-class PeakParameter : public Steinberg::Vst::Parameter
+//------------------------------------------------------------------------
+//------------------------------------------------------------------------
+class PeakParameter : public Parameter
 {
 public:
-	PeakParameter (int32 flags, int32 id, const Steinberg::Vst::TChar* title);
+	PeakParameter (int32 flags, int32 id, const TChar* title);
 
-	virtual void toString (Steinberg::Vst::ParamValue normValue, Steinberg::Vst::String128 string) const;
-	virtual bool fromString (const Steinberg::Vst::TChar* string, Steinberg::Vst::ParamValue& normValue) const;
+	virtual void toString (ParamValue normValue, String128 string) const;
+	virtual bool fromString (const TChar* string, ParamValue& normValue) const;
 };
 
 //------------------------------------------------------------------------
-PeakParameter::PeakParameter (int32 flags, int32 id, const Steinberg::Vst::TChar* title)
+PeakParameter::PeakParameter (int32 flags, int32 id, const TChar* title)
 {
 	UString (info.title, USTRINGSIZE (info.title)).assign (title);
 	
@@ -42,13 +47,13 @@ PeakParameter::PeakParameter (int32 flags, int32 id, const Steinberg::Vst::TChar
 	info.id = id;
 	info.stepCount = 0;
 	info.defaultNormalizedValue = 0.5f;
-	info.unitId = Steinberg::Vst::kRootUnitId;
+	info.unitId = kRootUnitId;
 	
 	setNormalized (1.f);
 }
 
 //------------------------------------------------------------------------
-void PeakParameter::toString (Steinberg::Vst::ParamValue normValue, Steinberg::Vst::String128 string) const
+void PeakParameter::toString (ParamValue normValue, String128 string) const
 {
 	String str;
 	if (normValue > 0.0001)
@@ -65,7 +70,7 @@ void PeakParameter::toString (Steinberg::Vst::ParamValue normValue, Steinberg::V
 }
 
 //------------------------------------------------------------------------
-bool PeakParameter::fromString (const Steinberg::Vst::TChar* string, Steinberg::Vst::ParamValue& normValue) const
+bool PeakParameter::fromString (const TChar* string, ParamValue& normValue) const
 {
 	return false;
 }
@@ -73,194 +78,143 @@ bool PeakParameter::fromString (const Steinberg::Vst::TChar* string, Steinberg::
 //------------------------------------------------------------------------
 //------------------------------------------------------------------------
 //------------------------------------------------------------------------
-class UIDescriptionTestController : public Steinberg::Vst::EditController
+tresult PLUGIN_API UIDescriptionBaseController::initialize (FUnknown* context)
 {
-public:
-	UIDescriptionTestController ()
+	tresult res = EditController::initialize (context);
+	if (res == kResultTrue)
 	{
+		// add processing parameters
+		PeakParameter* peakParam = new PeakParameter (ParameterInfo::kIsReadOnly, kPeakParam, USTRING("Peak"));
+		parameters.addParameter (peakParam);
 	}
-	
-	tresult PLUGIN_API initialize (FUnknown* context)
-	{
-		tresult res = EditController::initialize (context);
-		if (res == kResultTrue)
-		{
-			// add processing parameters
-			PeakParameter* peakParam = new PeakParameter (Steinberg::Vst::ParameterInfo::kIsReadOnly, kPeakParam, USTRING("Peak"));
-			parameters.addParameter (peakParam);
-			
-			// add ui parameters
-			Steinberg::Vst::StringListParameter* slp = new Steinberg::Vst::StringListParameter (USTRING("TabController"), 20000);
-			slp->appendString (USTRING("Tab1"));
-			slp->appendString (USTRING("Tab2"));
-			slp->appendString (USTRING("Tab3"));
-			slp->appendString (USTRING("Tab4"));
-			slp->appendString (USTRING("Tab5"));
-			uiParameters.addParameter (slp);
-		}
-		return res;
-	}
-
-	IPlugView* PLUGIN_API createView (FIDString name)
-	{
-		if (strcmp (name, Steinberg::Vst::ViewType::kEditor) == 0)
-		{
-			return new VST3Editor (this, "view", "myEditor.uidesc");
-		}
-		return 0;
-	}
-
-	Steinberg::Vst::Parameter* getParameterObject (Steinberg::Vst::ParamID tag)
-	{
-		Steinberg::Vst::Parameter* param = Steinberg::Vst::EditController::getParameterObject (tag);
-		if (param == 0)
-		{
-			param = uiParameters.getParameter (tag);
-		}
-		return param;
-	}
-
-	// make sure that our UI only parameters doesn't call the following three EditController methods: beginEdit, endEdit, performEdit
-	tresult beginEdit (Steinberg::Vst::ParamID tag)
-	{
-		if (Steinberg::Vst::EditController::getParameterObject (tag))
-			return Steinberg::Vst::EditController::beginEdit (tag);
-		return kResultFalse;
-	}
-	
-	tresult performEdit (Steinberg::Vst::ParamID tag, Steinberg::Vst::ParamValue valueNormalized)
-	{
-		if (Steinberg::Vst::EditController::getParameterObject (tag))
-			return EditController::performEdit (tag, valueNormalized);
-		return kResultFalse;
-	}
-	
-	tresult endEdit (Steinberg::Vst::ParamID tag)
-	{
-		if (Steinberg::Vst::EditController::getParameterObject (tag))
-			return Steinberg::Vst::EditController::endEdit (tag);
-		return kResultFalse;
-	}
-
-	static FUnknown* createInstance (void*) { return (IEditController*)new UIDescriptionTestController; }
-	static FUID cid;
-protected:
-	Steinberg::Vst::ParameterContainer uiParameters;
-};
-FUID UIDescriptionTestController::cid (0xF0FF3C24, 0x3F2F4C94, 0x84F6B6AE, 0xEF7BF28B);
+	return res;
+}
 
 //------------------------------------------------------------------------
-//------------------------------------------------------------------------
-//------------------------------------------------------------------------
-class UIDescriptionTestProcessor : public Steinberg::Vst::AudioEffect
+Parameter* UIDescriptionBaseController::getParameterObject (ParamID tag)
 {
-public:
-	UIDescriptionTestProcessor ()
+	Parameter* param = EditController::getParameterObject (tag);
+	if (param == 0)
 	{
-		setControllerClass (UIDescriptionTestController::cid);
+		param = uiParameters.getParameter (tag);
 	}
-	
-	tresult PLUGIN_API initialize (FUnknown* context)
-	{
-		tresult res = AudioEffect::initialize (context);
-		if (res == kResultTrue)
-		{
-			addAudioInput (USTRING("Audio Input"), Steinberg::Vst::SpeakerArr::kStereo);
-			addAudioOutput (USTRING("Audio Output"), Steinberg::Vst::SpeakerArr::kStereo);
-		}
-		return res;
-	}
+	return param;
+}
 
-	tresult PLUGIN_API setBusArrangements (Steinberg::Vst::SpeakerArrangement* inputs, int32 numIns, Steinberg::Vst::SpeakerArrangement* outputs, int32 numOuts)
-	{
-		if (numIns != 1 || numOuts != 1)
-			return kResultFalse;
-		if (inputs[0] != outputs[0])
-			return kResultFalse;
-		return Steinberg::Vst::AudioEffect::setBusArrangements (inputs, numIns, outputs, numOuts);
-	}
-	
-	tresult PLUGIN_API process (Steinberg::Vst::ProcessData& data)
-	{
-		Steinberg::Vst::ParamValue peak = 0.;
-		for (int32 sample = 0; sample < data.numSamples; sample++)
-		{
-			for (int32 channel = 0; channel < data.inputs[0].numChannels; channel++)
-			{
-				float value = data.inputs[0].channelBuffers32[channel][sample];
-				data.outputs[0].channelBuffers32[channel][sample] = value;
-				value = fabs (value);
-				if (value > peak)
-					peak = value;
-			}
-		}
-		if (data.outputParameterChanges)
-		{
-			int32 index;
-			Steinberg::Vst::IParamValueQueue* queue = data.outputParameterChanges->addParameterData (kPeakParam, index);
-			if (queue)
-				queue->addPoint (0, peak, index);
-		}
-		return kResultTrue;
-	}
+// make sure that our UI only parameters doesn't call the following three EditController methods: beginEdit, endEdit, performEdit
+//------------------------------------------------------------------------
+tresult UIDescriptionBaseController::beginEdit (ParamID tag)
+{
+	if (EditController::getParameterObject (tag))
+		return EditController::beginEdit (tag);
+	return kResultFalse;
+}
 
-	static FUnknown* createInstance (void*) { return (IAudioProcessor*)new UIDescriptionTestProcessor; }
-	static FUID cid;
-};
-FUID UIDescriptionTestProcessor::cid (0x49BAF003, 0xB44D455E, 0x9CBDE54F, 0x7FF2CBA1);
+//------------------------------------------------------------------------
+tresult UIDescriptionBaseController::performEdit (ParamID tag, ParamValue valueNormalized)
+{
+	if (EditController::getParameterObject (tag))
+		return EditController::performEdit (tag, valueNormalized);
+	return kResultFalse;
+}
 
+//------------------------------------------------------------------------
+tresult UIDescriptionBaseController::endEdit (ParamID tag)
+{
+	if (EditController::getParameterObject (tag))
+		return EditController::endEdit (tag);
+	return kResultFalse;
+}
+
+//------------------------------------------------------------------------
+//------------------------------------------------------------------------
+//------------------------------------------------------------------------
+UIDescriptionTestController::UIDescriptionTestController ()
+{
+}
+
+//------------------------------------------------------------------------
+tresult PLUGIN_API UIDescriptionTestController::initialize (FUnknown* context)
+{
+	tresult res = UIDescriptionBaseController::initialize (context);
+	if (res == kResultTrue)
+	{
+		// add ui parameters
+		StringListParameter* slp = new StringListParameter (USTRING("TabController"), 20000);
+		slp->appendString (USTRING("Tab1"));
+		slp->appendString (USTRING("Tab2"));
+		slp->appendString (USTRING("Tab3"));
+		slp->appendString (USTRING("Tab4"));
+		slp->appendString (USTRING("Tab5"));
+		uiParameters.addParameter (slp);
+	}
+	return res;
+}
+
+//------------------------------------------------------------------------
+IPlugView* PLUGIN_API UIDescriptionTestController::createView (FIDString name)
+{
+	if (strcmp (name, ViewType::kEditor) == 0)
+	{
+		return new VST3Editor (this, "view", "myEditor.uidesc");
+	}
+	return 0;
+}
+
+//------------------------------------------------------------------------
+//------------------------------------------------------------------------
+//------------------------------------------------------------------------
+UIDescriptionTestProcessor::UIDescriptionTestProcessor ()
+{
+	setControllerClass (UIDescriptionTestController::cid);
+}
+
+//------------------------------------------------------------------------
+tresult PLUGIN_API UIDescriptionTestProcessor::initialize (FUnknown* context)
+{
+	tresult res = AudioEffect::initialize (context);
+	if (res == kResultTrue)
+	{
+		addAudioInput (USTRING("Audio Input"), SpeakerArr::kStereo);
+		addAudioOutput (USTRING("Audio Output"), SpeakerArr::kStereo);
+	}
+	return res;
+}
+
+//------------------------------------------------------------------------
+tresult PLUGIN_API UIDescriptionTestProcessor::setBusArrangements (SpeakerArrangement* inputs, int32 numIns, SpeakerArrangement* outputs, int32 numOuts)
+{
+	if (numIns != 1 || numOuts != 1)
+		return kResultFalse;
+	if (inputs[0] != outputs[0])
+		return kResultFalse;
+	return AudioEffect::setBusArrangements (inputs, numIns, outputs, numOuts);
+}
+
+//------------------------------------------------------------------------
+tresult PLUGIN_API UIDescriptionTestProcessor::process (ProcessData& data)
+{
+	ParamValue peak = 0.;
+	for (int32 sample = 0; sample < data.numSamples; sample++)
+	{
+		for (int32 channel = 0; channel < data.inputs[0].numChannels; channel++)
+		{
+			float value = data.inputs[0].channelBuffers32[channel][sample];
+			data.outputs[0].channelBuffers32[channel][sample] = value;
+			value = fabs (value);
+			if (value > peak)
+				peak = value;
+		}
+	}
+	if (data.outputParameterChanges)
+	{
+		int32 index;
+		IParamValueQueue* queue = data.outputParameterChanges->addParameterData (kPeakParam, index);
+		if (queue)
+			queue->addPoint (0, peak, index);
+	}
+	return kResultTrue;
+}
 
 } // namespace VSTGUI
 
-//------------------------------------------------------------------------
-//------------------------------------------------------------------------
-//------------------------------------------------------------------------
-BEGIN_FACTORY("VSTGUI", "", "", PFactoryInfo::kUnicode)
-
-	DEF_CLASS2 (INLINE_UID_FROM_FUID(UIDescriptionTestProcessor::cid),
-				PClassInfo::kManyInstances,
-				kVstAudioEffectClass,
-				"VSTGUI UIDescription Test",
-				Vst::kDistributable,
-				"Fx",
-				"1.0.0",
-				kVstVersionString,
-				UIDescriptionTestProcessor::createInstance)
-				
-	DEF_CLASS2 (INLINE_UID_FROM_FUID(UIDescriptionTestController::cid),
-				PClassInfo::kManyInstances,
-				kVstComponentControllerClass,
-				"VSTGUI UIDescription Test",
-				Vst::kDistributable,
-				"Fx",
-				"1.0.0",
-				kVstVersionString,
-				UIDescriptionTestController::createInstance)
-				
-END_FACTORY
-
-//------------------------------------------------------------------------
-//------------------------------------------------------------------------
-//------------------------------------------------------------------------
-void* hInstance = 0;
-
-//------------------------------------------------------------------------
-bool InitModule ()   
-{
-	extern void* moduleHandle;
-	hInstance = moduleHandle;
-
-	return true; 
-}
-
-//------------------------------------------------------------------------
-bool DeinitModule ()
-{
-	return true; 
-}
-
-//------------------------------------------------------------------------
-AudioEffect* createEffectInstance (audioMasterCallback audioMaster)
-{
-	return Steinberg::Vst::Vst2Wrapper::create (GetPluginFactory (), UIDescriptionTestProcessor::cid, '????', audioMaster);
-}
