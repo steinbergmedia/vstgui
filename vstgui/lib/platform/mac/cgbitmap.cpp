b82abbef037f0363a1a6d621f97021a07d3358f3
scheffle <scheffle@users.noreply.github.com>
2010-09-25 07:19:27 +0000

    * workaround bug on Mac OS X that PNG images are always decoded when drawn

--- vstgui/lib/platform/mac/cgbitmap.cpp
+++ vstgui/lib/platform/mac/cgbitmap.cpp
@@ -132,7 +132,10 @@ bool CGBitmap::load (const CResourceDescription& desc)
 	return result;
 }
 
+//-----------------------------------------------------------------------------
 static CFStringRef kCGImageSourceShouldPreferRGB32 = CFSTR("kCGImageSourceShouldPreferRGB32");
+
+#define VSTGUI_QUARTZ_WORKAROUND_PNG_DECODE_ON_DRAW_BUG __i386__
 
 //-----------------------------------------------------------------------------
 bool CGBitmap::loadFromImageSource (CGImageSourceRef source)
@@ -157,8 +160,19 @@ bool CGBitmap::loadFromImageSource (CGImageSourceRef source)
 			if (CFNumberGetValue (value, kCFNumberDoubleType, &fValue))
 				size.x = fValue;
 		}
-		CFRelease (properties);
 		CFRetain (imageSource);
+	#if VSTGUI_QUARTZ_WORKAROUND_PNG_DECODE_ON_DRAW_BUG
+		// workaround a bug in Mac OS X 10.6 (32 bit), where PNG bitmaps were decoded all the time when drawn.
+		// we fix this by copying the pixels of the bitmap into our own buffer.
+		CFStringRef imageType = CGImageSourceGetType (imageSource);
+		if (imageType && CFStringCompare (imageType, CFSTR("public.png"), 0) == kCFCompareEqualTo)
+		{
+			CGContextRef context = createCGContext ();
+			if (context)
+				CFRelease (context);
+		}
+	#endif
+		CFRelease (properties);
 	}
 	return (size.x != 0 && size.y != 0);
 }
