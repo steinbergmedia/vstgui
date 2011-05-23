90115ac5380792e613d34d80ce660d2c2f25d260
scheffle <scheffle@users.noreply.github.com>
2011-05-23 17:03:19 +0000

    add missing include + fix overloaded method

--- vstgui/lib/cscrollview.h
+++ vstgui/lib/cscrollview.h
@@ -1,6 +1,6 @@
 //-----------------------------------------------------------------------------
 // VST Plug-Ins SDK
-// VSTGUI: Graphical User Interface Framework for VST plugins : 
+// VSTGUI: Graphical User Interface Framework for VST plugins :
 //
 // Version 4.0
 //
@@ -10,24 +10,24 @@
 //-----------------------------------------------------------------------------
 // Redistribution and use in source and binary forms, with or without modification,
 // are permitted provided that the following conditions are met:
-// 
-//   * Redistributions of source code must retain the above copyright notice, 
+//
+//   * Redistributions of source code must retain the above copyright notice,
 //     this list of conditions and the following disclaimer.
 //   * Redistributions in binary form must reproduce the above copyright notice,
-//     this list of conditions and the following disclaimer in the documentation 
+//     this list of conditions and the following disclaimer in the documentation
 //     and/or other materials provided with the distribution.
 //   * Neither the name of the Steinberg Media Technologies nor the names of its
-//     contributors may be used to endorse or promote products derived from this 
+//     contributors may be used to endorse or promote products derived from this
 //     software without specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A  PARTICULAR PURPOSE ARE DISCLAIMED. 
-// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
-// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
-// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
-// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A  PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 // OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE  OF THIS SOFTWARE, EVEN IF ADVISED
 // OF THE POSSIBILITY OF SUCH DAMAGE.
 //-----------------------------------------------------------------------------
@@ -52,21 +52,21 @@ class CVSTGUITimer;
 class CScrollView : public CViewContainer, public CControlListener
 {
 protected:
-	enum 
+	enum
 	{
 		kHorizontalScrollbarFlag,
 		kVerticalScrollbarFlag,
 		kDontDrawFrameFlag,
 		kAutoDragScollingFlag,
-		
+
 		kLastScrollViewStyleFlag
 	};
-	
+
 public:
 	CScrollView (const CRect& size, const CRect& containerSize, CFrame* pParent, int32_t style, CCoord scrollbarWidth = 16, CBitmap* pBackground = 0);
 	CScrollView (const CScrollView& scrollView);
 
-	enum CScrollViewStyle 
+	enum CScrollViewStyle
 	{
 		kHorizontalScrollbar	= 1 << kHorizontalScrollbarFlag,	///< add a horizontal scrollbar
 		kVerticalScrollbar 		= 1 << kVerticalScrollbarFlag,		///< add a vertical scrollbar
@@ -80,23 +80,23 @@ public:
 	//@{
 	int32_t getStyle () const { return style; }
 	void setStyle (int32_t newStyle);
-	
+
 	CCoord getScrollbarWidth () const { return scrollbarWidth; }
 	void setScrollbarWidth (CCoord width);
-	
+
 	virtual void setContainerSize (const CRect& cs, bool keepVisibleArea = false);	///< set the virtual size of this container
 	const CRect& getContainerSize () const { return containerSize; }
 	const CPoint& getScrollOffset () const;				///< get scroll offset
-	
+
 	CScrollbar* getVerticalScrollbar () const { return vsb; }	///< get the vertical scrollbar
 	CScrollbar* getHorizontalScrollbar () const { return hsb; }	///< get the horizontal scrollbar
 
 	virtual void makeRectVisible (const CRect& rect);	///< set scrollview to show rect
 	//@}
-	
+
 	// overwrite
 	bool addView (CView* pView);
-	bool addView (CView* pView, CRect& mouseableArea, bool mouseEnabled = true);
+	bool addView (CView* pView, const CRect& mouseableArea, bool mouseEnabled = true);
 	bool addView (CView* pView, CView* pBefore);
 	bool removeView (CView* pView, bool withForget = true);
 	bool removeAll (bool withForget = true);
@@ -192,13 +192,13 @@ protected:
 	CRect scrollSize;
 	CRect scrollerArea;
 
-	float stepValue;	
+	float stepValue;
 	CCoord scrollerLength;
 
 	CColor frameColor;
 	CColor scrollerColor;
 	CColor backgroundColor;
-	
+
 	IScrollbarDrawer* drawer;
 private:
 	~CScrollbar ();
