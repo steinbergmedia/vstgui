80637d7442606b9c9901b0969b7977ae7181ad66
scheffle <scheffle@users.noreply.github.com>
2011-05-23 17:05:16 +0000

    add missing include

--- vstgui/uidescription/cstream.h
+++ vstgui/uidescription/cstream.h
@@ -1,6 +1,6 @@
 //-----------------------------------------------------------------------------
 // VST Plug-Ins SDK
-// VSTGUI: Graphical User Interface Framework not only for VST plugins : 
+// VSTGUI: Graphical User Interface Framework not only for VST plugins :
 //
 // Version 4.0
 //
@@ -10,24 +10,24 @@
 //-----------------------------------------------------------------------------
 // Redistribution and use in source and binary forms, with or without modification,
 // are permitted provided that the following conditions are met:
-// 
-//   * Redistributions of source code must retain the above copyright notice, 
+//
+//   * Redistributions of source code must retain the above copyright notice,
 //     this list of conditions and the following disclaimer.
 //   * Redistributions in binary form must reproduce the above copyright notice,
-//     this list of conditions and the following disclaimer in the documentation 
+//     this list of conditions and the following disclaimer in the documentation
 //     and/or other materials provided with the distribution.
 //   * Neither the name of the Steinberg Media Technologies nor the names of its
-//     contributors may be used to endorse or promote products derived from this 
+//     contributors may be used to endorse or promote products derived from this
 //     software without specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A  PARTICULAR PURPOSE ARE DISCLAIMED. 
-// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
-// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
-// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
-// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A  PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 // OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE  OF THIS SOFTWARE, EVEN IF ADVISED
 // OF THE POSSIBILITY OF SUCH DAMAGE.
 //-----------------------------------------------------------------------------
@@ -36,6 +36,7 @@
 #define __cstream__
 
 #include "../lib/vstguibase.h"
+#include "../lib/cbitmap.h"
 #include <string>
 
 namespace VSTGUI {
@@ -50,7 +51,7 @@ public:
 
 	ByteOrder getByteOrder () const { return byteOrder; }
 	void setByteOrder (ByteOrder newByteOrder) { byteOrder = newByteOrder; }
-	
+
 	bool operator<< (const int8_t& input);
 	bool operator<< (const uint8_t& input);
 	bool operator<< (const int16_t& input);
@@ -75,10 +76,10 @@ class InputStream
 {
 public:
 	InputStream (ByteOrder byteOrder = kNativeByteOrder) : byteOrder (byteOrder) {}
-	
+
 	ByteOrder getByteOrder () const { return byteOrder; }
 	void setByteOrder (ByteOrder newByteOrder) { byteOrder = newByteOrder; }
-	
+
 	bool operator>> (int8_t& output);
 	bool operator>> (uint8_t& output);
 	bool operator>> (int16_t& output);
@@ -107,7 +108,7 @@ public:
 		kSeekCurrent,
 		kSeekEnd
 	};
-	
+
 	virtual int64_t seek (int64_t pos, SeekMode mode) = 0;	///< returns -1 if seek fails otherwise new position
 	virtual int64_t tell () const = 0;
 	virtual void rewind () = 0;
@@ -122,7 +123,7 @@ public:
 	CMemoryStream (int32_t initialSize = 1024, int32_t delta = 1024, bool binaryMode = true, ByteOrder byteOrder = kNativeByteOrder);
 	CMemoryStream (const int8_t* buffer, int32_t bufferSize, bool binaryMode = true, ByteOrder byteOrder = kNativeByteOrder);
 	~CMemoryStream ();
-	
+
 	int32_t writeRaw (const void* buffer, int32_t size);
 	int32_t readRaw (void* buffer, int32_t size);
 
@@ -160,9 +161,9 @@ public:
 		kTruncateMode	= 1 << 2,
 		kBinaryMode		= 1 << 3
 	};
-	
+
 	bool open (UTF8StringPtr path, int32_t mode, ByteOrder byteOrder = kNativeByteOrder);
-	
+
 	int32_t writeRaw (const void* buffer, int32_t size);
 	int32_t readRaw (void* buffer, int32_t size);
 
@@ -185,7 +186,7 @@ class CResourceInputStream : public InputStream, public SeekableStream
 public:
 	CResourceInputStream (ByteOrder byteOrder = kNativeByteOrder);
 	~CResourceInputStream ();
-	
+
 	bool open (const CResourceDescription& res);
 
 	virtual bool operator>> (std::string& string) { return false; }
@@ -194,7 +195,7 @@ public:
 	virtual int64_t tell () const;
 	virtual void rewind ();
 protected:
-	void* platformHandle;	
+	void* platformHandle;
 };
 
 
