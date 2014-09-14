e99fd36fd7d497f60a0c15563d126fbebf4efe91
scheffle <scheffle@users.noreply.github.com>
2014-09-14 15:56:09 +0000

    add iOS standalone app test

---
+++ vstgui/tests/ios standalone/iOS Standalone/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iOS Standalone
+//
+//  Created by Arne Scheffler on 12/09/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
