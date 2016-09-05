77c62f5eb06b7a4125628cd0a864d3edaf1c2bba
scheffle <scheffle@users.noreply.github.com>
2016-09-05 09:15:41 +0200

    documentation

--- vstgui/standalone/iasync.h
+++ vstgui/standalone/iasync.h
@@ -11,20 +11,22 @@ namespace Async {
 using Task = std::function<void ()>;
 
 //------------------------------------------------------------------------
-/** Asynchronous Context */
+/** Asynchronous context. */
 enum class Context
 {
+	/** Main thread context. */
 	Main,
+	/** Background thread context. */
 	Background
 };
 
 //------------------------------------------------------------------------
-/** Perform a task asynchronously.
- *
- *	Schedule a task to be performed asynchronous either on a background thread or on the main
+/** Schedule a task to be performed asynchronous either on a background thread or on the main
  *	thread.
  *
- *	Can be called from any thread.
+ *	Can be called from any thread except realtime constraint threads.
+ *
+ *	@ingroup standalone
  *
  *	@param context	background or main thread
  *	@param task		task to be performed
