d83eae87877e804fc9f744f61e7a9ad48677f239
scheffle <scheffle@users.noreply.github.com>
2017-04-17 09:39:19 +0200

    add subgroup feature and setFloat method to preferences helper

--- vstgui/standalone/include/helpers/preferences.h
+++ vstgui/standalone/include/helpers/preferences.h
@@ -7,30 +7,67 @@
 namespace VSTGUI {
 namespace Standalone {
 
+static const char* DefaultPreferencesGroupSeparator = "::";
+
 //------------------------------------------------------------------------
 class Preferences
 {
 public:
-	Preferences () : preferences (IApplication::instance ().getPreferences ()) {}
+	Preferences (Preferences&&) = default;
+	Preferences& operator=(Preferences&&) = default;
 	
-	inline bool set (const UTF8String& key, const UTF8String& value)
+	Preferences (const std::initializer_list<const char*>& groups, const char* groupSeparator = DefaultPreferencesGroupSeparator)
+	: groupSeparator (groupSeparator)
 	{
+		for (auto& g : groups)
+			groupKey += UTF8String (g) + groupSeparator;
+	}
+
+	Preferences (const UTF8String& inGroupKey = "", const char* groupSeparator = DefaultPreferencesGroupSeparator)
+	: groupKey (inGroupKey)
+	, groupSeparator (groupSeparator)
+	{
+		if (!groupKey.empty ())
+			groupKey += groupSeparator;
+	}
+
+	inline Preferences subGroupPreferences (const UTF8String& subGroup) const
+	{
+		return Preferences (groupKey + subGroup, groupSeparator);
+	}
+	
+	inline bool set (const UTF8String& key, const UTF8String& value) const
+	{
+		if (!groupKey.empty ())
+			return preferences.set (groupKey + key, value);
 		return preferences.set (key, value);
 	}
 
-	inline Optional<UTF8String> get (const UTF8String& key)
+	inline Optional<UTF8String> get (const UTF8String& key) const
 	{
+		if (!groupKey.empty ())
+			return preferences.get (groupKey + key);
 		return preferences.get (key);
 	}
 	
 	template<typename T>
-	inline bool setNumber (const UTF8String& key, T value)
+	inline bool setNumber (const UTF8String& key, T value) const
 	{
 		return set (key, toString (value));
 	}
 
 	template<typename T>
-	inline Optional<T> getNumber (const UTF8String& key)
+	inline bool setFloat (const UTF8String& key, T value, uint32_t precision = 8) const
+	{
+		std::ostringstream sstream;
+		sstream.imbue (std::locale::classic ());
+		sstream.precision (static_cast<std::streamsize> (precision));
+		sstream << value;
+		return set (key, UTF8String (sstream.str ()));
+	}
+	
+	template<typename T>
+	inline Optional<T> getNumber (const UTF8String& key) const
 	{
 		if (auto p = get (key))
 		{
@@ -39,7 +76,7 @@ public:
 		return {};
 	}
 
-	inline bool setPoint (const UTF8String& key, CPoint p, uint32_t precision = 8)
+	inline bool setPoint (const UTF8String& key, CPoint p, uint32_t precision = 8) const
 	{
 		std::ostringstream sstream;
 		sstream.imbue (std::locale::classic ());
@@ -52,7 +89,7 @@ public:
 		return set (key, UTF8String (sstream.str ()));
 	}
 	
-	inline Optional<CPoint> getPoint (const UTF8String& key)
+	inline Optional<CPoint> getPoint (const UTF8String& key) const
 	{
 		if (auto p = get (key))
 		{
@@ -77,8 +114,15 @@ public:
 		return {};
 	}
 	
+	inline const UTF8String& getGroupKey () const { return groupKey; }
+	inline const UTF8String& getGroupSeparator () const { return groupSeparator; }
 private:
-	IPreference& preferences;
+	Preferences (const Preferences&) = default;
+	Preferences& operator=(const Preferences&) = default;
+
+	IPreference& preferences {IApplication::instance ().getPreferences ()};
+	UTF8String groupKey;
+	UTF8String groupSeparator;
 };
 
 //------------------------------------------------------------------------
