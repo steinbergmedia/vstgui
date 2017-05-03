1820c144de10f48eee8302e9666a7e0ac739d06e
scheffle <scheffle@users.noreply.github.com>
2017-05-03 18:08:21 +0200

    add license file and mark new standalone files to use that license

--- vstgui/standalone/examples/standalone/source/testappdelegate.cpp
+++ vstgui/standalone/examples/standalone/source/testappdelegate.cpp
@@ -1,3 +1,7 @@
+// This file is part of VSTGUI. It is subject to the license terms 
+// in the LICENSE file found in the top-level directory of this
+// distribution and at http://github.com/steinbergmedia/vstgui/LICENSE
+
 #include "testappdelegate.h"
 #include "testmodel.h"
 #include "testabout.h"
