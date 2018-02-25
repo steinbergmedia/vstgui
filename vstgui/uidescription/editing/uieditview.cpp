c9034e38ba2785cb5f6ca4f9be61b80b9ce273d0
scheffle <scheffle@users.noreply.github.com>
2018-02-25 13:06:41 +0100

    remove old pre c++11 code

--- vstgui/uidescription/editing/uieditview.cpp
+++ vstgui/uidescription/editing/uieditview.cpp
@@ -24,16 +24,6 @@
 #include "../../lib/idatapackage.h"
 #include <cassert>
 
-#if __cplusplus < 201103L
-namespace std {
-#if _MSC_VER < 1800
-	template <typename T> T round (T value) { return floor (value + 0.5); }
-#else
-	using ::round;
-#endif
-}
-#endif
-
 namespace VSTGUI {
 
 namespace UIEditViewInternal {
