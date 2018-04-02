d6532b8c280a395104da1f089545e55a9f81e7be
scheffle <scheffle@users.noreply.github.com>
2018-04-02 10:30:50 +0200

    insert copyright header

--- vstgui/tools/imagestitcher/source/documentcontroller.cpp
+++ vstgui/tools/imagestitcher/source/documentcontroller.cpp
@@ -1,3 +1,7 @@
+// This file is part of VSTGUI. It is subject to the license terms
+// in the LICENSE file found in the top-level directory of this
+// distribution and at http://github.com/steinbergmedia/vstgui/LICENSE
+
 #include "documentcontroller.h"
 #include "imageframesview.h"
 #include "vstgui/lib/cbitmap.h"
--- vstgui/tools/imagestitcher/source/imageframesview.cpp
+++ vstgui/tools/imagestitcher/source/imageframesview.cpp
@@ -1,3 +1,6 @@
+// This file is part of VSTGUI. It is subject to the license terms
+// in the LICENSE file found in the top-level directory of this
+// distribution and at http://github.com/steinbergmedia/vstgui/LICENSE
 
 #include "imageframesview.h"
 #include "vstgui/lib/cdrawcontext.h"
