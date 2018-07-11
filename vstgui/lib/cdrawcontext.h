fef379f284e054b11d076f03f1e85483ba9d7fcd
Reimund Dratwa <rd@fuseaudiolabs.de>
2018-07-11 18:46:09 +0200

    Integrating changes suggested by Arne.

--- vstgui/lib/cdrawcontext.h
+++ vstgui/lib/cdrawcontext.h
@@ -218,7 +218,7 @@ protected:
 		CLineStyle lineStyle {kLineOnOffDash};
 		CDrawMode drawMode {kAntiAliasing};
 		float globalAlpha {1.f};
-		BitmapInterpolationQuality bitmapQuality {kQualityDefault};
+		BitmapInterpolationQuality bitmapQuality {BitmapInterpolationQuality::kDefault};
 
 		CDrawContextState () = default;
 		CDrawContextState (const CDrawContextState& state);
