7bf165d90a14e6fd8b44074b14636361c7de62b1
Yvan Grabit <ygrabit@users.noreply.github.com>
2018-12-14 12:03:31 +0100

    [fix] Compilation for Visual of the UI Test

--- vstgui/tests/uidescription vst3/source/vst3sdksources.cpp
+++ vstgui/tests/uidescription vst3/source/vst3sdksources.cpp
@@ -5,31 +5,11 @@
 #if MAC
 #include <CoreFoundation/CoreFoundation.h>
 #endif
-
-#include "public.sdk/source/vst/vst2wrapper/vst2wrapper.sdk.cpp"
-#include "public.sdk/source/common/pluginview.cpp"
-#include "public.sdk/source/main/pluginfactoryvst3.cpp"
-#include "public.sdk/source/vst3stdsdk.cpp"
-#include "public.sdk/source/vst/vstguieditor.cpp"
-#include "public.sdk/source/vst/vstinitiids.cpp"
-#include "base/source/baseiids.cpp"
-#include "base/source/classfactory.cpp"
-#include "base/source/fatomic.cpp"
-#include "base/source/fbuffer.cpp"
-#include "base/source/fdebug.cpp"
-#include "base/source/fobject.cpp"
-#include "base/source/fstreamer.cpp"
-#include "base/source/fstring.cpp"
-#include "base/source/fthread.cpp"
-#include "base/source/timer.cpp"
-#include "base/source/updatehandler.cpp"
-#include "pluginterfaces/base/conststringtable.cpp"
-#include "pluginterfaces/base/funknown.cpp"
-#include "pluginterfaces/base/ustring.cpp"
+#include "pluginterfaces/base/fplatform.h"
 
 #if MAC
-	#include "public.sdk/source/main/macmain.cpp"
+#include "public.sdk/source/main/macmain.cpp"
 #endif
 #if WINDOWS
-	#include "public.sdk/source/main/dllmain.cpp"
+#include "public.sdk/source/main/dllmain.cpp"
 #endif
