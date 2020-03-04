a0eaf62c25d3dfc7d52859f5a61f20a045aeac3b
scheffle <scheffle@users.noreply.github.com>
2020-03-04 18:36:14 +0100

    make it possible to build with clang for Windows

--- vstgui/standalone/source/platform/win32/win32application.cpp
+++ vstgui/standalone/source/platform/win32/win32application.cpp
@@ -24,10 +24,12 @@
 #pragma comment(lib, "d2d1.lib")
 #pragma comment(lib, "dwrite.lib")
 
+#ifndef __clang__
 #pragma comment(linker, \
                 "\"/manifestdependency:type='win32' \
 name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
 processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#endif
 
 //------------------------------------------------------------------------
 namespace VSTGUI {
