a5700637eb2bea1b1d6bd9e6ae4f92fe7dfc2a23
scheffle <scheffle@users.noreply.github.com>
2020-03-27 13:42:16 +0100

    add CommonDirectories::AppResourcesPath

--- vstgui/standalone/source/platform/win32/win32commondirectories.h
+++ vstgui/standalone/source/platform/win32/win32commondirectories.h
@@ -1,4 +1,4 @@
-// This file is part of VSTGUI. It is subject to the license terms 
+// This file is part of VSTGUI. It is subject to the license terms
 // in the LICENSE file found in the top-level directory of this
 // distribution and at http://github.com/steinbergmedia/vstgui/LICENSE
 
@@ -18,12 +18,18 @@ class CommonDirectories : public ICommonDirectories
 public:
 	CommonDirectories ();
 
-	Optional<UTF8String> get (CommonDirectoryLocation location, const UTF8String& subDir, bool create = false) const override;
+	Optional<UTF8String> get (CommonDirectoryLocation location, const UTF8String& subDir,
+	                          bool create = false) const override;
+
+	void setAppResourcePath (const UTF8String& path);
+	
 private:
-	Optional<UTF8String> getLocalAppDataPath (const UTF8String& dir, const UTF8String& subDir, bool create) const;
+	Optional<UTF8String> getLocalAppDataPath (const UTF8String& dir, const UTF8String& subDir,
+	                                          bool create) const;
 	Optional<UTF8String> getAppPath () const;
 
 	UTF8String localAppDataPath;
+	UTF8String appResourcePath;
 };
 
 //------------------------------------------------------------------------
