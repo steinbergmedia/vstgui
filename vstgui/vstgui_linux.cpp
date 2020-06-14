0e6f57c6570ef47e843825e587147a3670d0fe6c
scheffle <scheffle@@users.noreply.github.com>
2020-06-14 12:13:06 +0200

    add missing files

--- vstgui/vstgui_linux.cpp
+++ vstgui/vstgui_linux.cpp
@@ -2,12 +2,16 @@
 
 #include "lib/platform/linux/linuxstring.cpp"
 
+#include "lib/platform/linux/x11fileselector.cpp"
 #include "lib/platform/linux/x11frame.cpp"
 #include "lib/platform/linux/x11platform.cpp"
 #include "lib/platform/linux/x11timer.cpp"
+#include "lib/platform/linux/x11utils.cpp"
 
 #include "lib/platform/linux/cairobitmap.cpp"
 #include "lib/platform/linux/cairocontext.cpp"
 #include "lib/platform/linux/cairofont.cpp"
 #include "lib/platform/linux/cairogradient.cpp"
 #include "lib/platform/linux/cairopath.cpp"
+
+#include "lib/platform/common/generictextedit.cpp"
