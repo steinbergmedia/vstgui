b0177af689c348dbc8f395a41c590f97f783911a
scheffle <scheffle@users.noreply.github.com>
2020-11-22 16:35:02 +0100

    add required code change to documentation

--- vstgui/doxygen/page_changes.h
+++ vstgui/doxygen/page_changes.h
@@ -110,6 +110,10 @@ Note: All current deprecated methods will be removed in the next version. So mak
 
 @section code_changes Changes for existing VSTGUI code
 
+@subsection code_changes_4_9_to_4_10 VSTGUI 4.9 -> VSTGUI 4.10
+
+- one has to use VSTGUI::initPlatform() before using VSTGUI and VSTGUI::exitPlatform() after use
+
 @subsection code_changes_4_8_to_4_9 VSTGUI 4.8 -> VSTGUI 4.9
 
 - removed method CView::onWheel (..) where the axis of the event was not included. You have to use the other onWheel method for your custom classes now.
