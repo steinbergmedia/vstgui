d42a28f9aca32d65153804580a8d0fb0411fac45
scheffle <scheffle@users.noreply.github.com>
2021-05-16 19:42:13 +0200

    Update idependency_test.cpp

--- vstgui/tests/unittest/lib/idependency_test.cpp
+++ vstgui/tests/unittest/lib/idependency_test.cpp
@@ -1,4 +1,4 @@
-// This file is part of VSTGUI. It is subject to the license terms 
+// This file is part of VSTGUI. It is subject to the license terms
 // in the LICENSE file found in the top-level directory of this
 // distribution and at http://github.com/steinbergmedia/vstgui/LICENSE
 
@@ -14,68 +14,60 @@ namespace VSTGUI {
 class DependentObject : public CBaseObject, public IDependency
 {
 public:
-	DependentObject ()
-	: notifyCalledCount (0)
-	{
-		
-	}
-	
+	DependentObject () : notifyCalledCount (0) {}
+
 	CMessageResult notify (CBaseObject* sender, IdStringPtr message) override
 	{
 		notifyCalledCount++;
 		return kMessageNotified;
 	}
-	
+
 	int32_t notifyCalledCount;
 };
 
 class TestObject : public CBaseObject, public IDependency
 {
 public:
-	TestObject ()
-	{
-		
-	}
-
+	TestObject () {}
 };
 
-TESTCASE(IDependencyTest,
+TEST_CASE (IDependencyTest, SimpleDependency)
+{
+	DependentObject dObj;
+	TestObject tObj;
+	tObj.addDependency (&dObj);
+	tObj.changed ("Test");
+	EXPECT (dObj.notifyCalledCount == 1)
+	tObj.removeDependency (&dObj);
+}
 
-	TEST(simpleDependency,
-		DependentObject dObj;
-		TestObject tObj;
-		tObj.addDependency (&dObj);
+TEST_CASE (IDependencyTest, SimpleDeferedDependency)
+{
+	DependentObject dObj;
+	TestObject tObj;
+	tObj.addDependency (&dObj);
+	tObj.deferChanges (true);
+	tObj.changed ("Test");
+	EXPECT (dObj.notifyCalledCount == 0)
+	tObj.deferChanges (false);
+	EXPECT (dObj.notifyCalledCount == 1)
+	tObj.removeDependency (&dObj);
+}
+
+TEST_CASE (IDependencyTest, SimpleDeferChanges)
+{
+	DependentObject dObj;
+	TestObject tObj;
+	tObj.addDependency (&dObj);
+	{
+		IDependency::DeferChanges df (&tObj);
 		tObj.changed ("Test");
-		EXPECT(dObj.notifyCalledCount == 1)
-		tObj.removeDependency(&dObj);
-	);
-
-	TEST(simpleDeferedDependency,
-		DependentObject dObj;
-		TestObject tObj;
-		tObj.addDependency (&dObj);
-		tObj.deferChanges (true);
 		tObj.changed ("Test");
-		EXPECT(dObj.notifyCalledCount == 0)
-		tObj.deferChanges (false);
-		EXPECT(dObj.notifyCalledCount == 1)
-		tObj.removeDependency (&dObj);
-	);
-
-	TEST(simpleDeferChanges,
-		DependentObject dObj;
-		TestObject tObj;
-		tObj.addDependency (&dObj);
-		{
-			IDependency::DeferChanges df (&tObj);
-			tObj.changed ("Test");
-			tObj.changed ("Test");
-			EXPECT(dObj.notifyCalledCount == 0)
-		}
-		EXPECT(dObj.notifyCalledCount == 1)
-		tObj.removeDependency (&dObj);
-	);
-);
+		EXPECT (dObj.notifyCalledCount == 0)
+	}
+	EXPECT (dObj.notifyCalledCount == 1)
+	tObj.removeDependency (&dObj);
+}
 
 } // VSTGUI
 
