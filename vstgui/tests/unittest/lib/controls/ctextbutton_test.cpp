013215a469a1c62d9e65f087acab4277c16207c2
scheffle <scheffle@users.noreply.github.com>
2021-05-24 12:44:29 +0200

    update tests

--- vstgui/tests/unittest/lib/controls/ctextbutton_test.cpp
+++ vstgui/tests/unittest/lib/controls/ctextbutton_test.cpp
@@ -1,4 +1,4 @@
-// This file is part of VSTGUI. It is subject to the license terms 
+// This file is part of VSTGUI. It is subject to the license terms
 // in the LICENSE file found in the top-level directory of this
 // distribution and at http://github.com/steinbergmedia/vstgui/LICENSE
 
@@ -7,119 +7,119 @@
 
 namespace VSTGUI {
 
-TESTCASE(CTextButtonTest,
+TEST_CASE (CTextButtonTest, MouseEventsKickStyle)
+{
+	auto b = owned (new CTextButton (CRect (10, 10, 50, 20)));
+	b->setStyle (CTextButton::kKickStyle);
+	b->setValue (b->getMin ());
+	CPoint p (10, 10);
+	EXPECT (b->onMouseDown (p, kRButton) == kMouseEventNotHandled);
+	EXPECT (b->isEditing () == false);
+	EXPECT (b->onMouseDown (p, kLButton) == kMouseEventHandled);
+	EXPECT (b->isEditing ());
+	EXPECT (b->getValue () == b->getMax ());
+	EXPECT (b->onMouseMoved (p, kLButton) == kMouseEventHandled);
+	EXPECT (b->isEditing ());
+	EXPECT (b->getValue () == b->getMax ());
+	EXPECT (b->onMouseUp (p, kLButton) == kMouseEventHandled);
+	EXPECT (b->isEditing () == false);
+	EXPECT (b->getValue () == b->getMin ());
 
-	TEST(mouseEventsKickStyle,
-		auto b = owned (new CTextButton (CRect (10, 10, 50, 20)));
-		b->setStyle (CTextButton::kKickStyle);
-		b->setValue (b->getMin ());
-		CPoint p (10, 10);
-		EXPECT (b->onMouseDown (p, kRButton) == kMouseEventNotHandled);
-		EXPECT (b->isEditing () == false);
-		EXPECT (b->onMouseDown (p, kLButton) == kMouseEventHandled);
-		EXPECT (b->isEditing ());
-		EXPECT (b->getValue () == b->getMax ());
-		EXPECT (b->onMouseMoved (p, kLButton) == kMouseEventHandled);
-		EXPECT (b->isEditing ());
-		EXPECT (b->getValue () == b->getMax ());
-		EXPECT (b->onMouseUp (p, kLButton) == kMouseEventHandled);
-		EXPECT (b->isEditing () == false);
-		EXPECT (b->getValue () == b->getMin ());
-		
-		EXPECT (b->onMouseDown (p, kLButton) == kMouseEventHandled);
-		EXPECT (b->isEditing ());
-		EXPECT (b->onMouseUp (p, kLButton) == kMouseEventHandled);
-		EXPECT (b->isEditing () == false);
-		EXPECT (b->getValue () == b->getMin ());
+	EXPECT (b->onMouseDown (p, kLButton) == kMouseEventHandled);
+	EXPECT (b->isEditing ());
+	EXPECT (b->onMouseUp (p, kLButton) == kMouseEventHandled);
+	EXPECT (b->isEditing () == false);
+	EXPECT (b->getValue () == b->getMin ());
 
-		EXPECT (b->onMouseDown (p, kLButton) == kMouseEventHandled);
-		EXPECT (b->isEditing ());
-		p (0, 0);
-		EXPECT (b->onMouseUp (p, kLButton) == kMouseEventHandled);
-		EXPECT (b->isEditing () == false);
-		EXPECT (b->getValue () == b->getMin ());
+	EXPECT (b->onMouseDown (p, kLButton) == kMouseEventHandled);
+	EXPECT (b->isEditing ());
+	p (0, 0);
+	EXPECT (b->onMouseUp (p, kLButton) == kMouseEventHandled);
+	EXPECT (b->isEditing () == false);
+	EXPECT (b->getValue () == b->getMin ());
 
-		p (10, 10);
-		EXPECT (b->onMouseDown (p, kLButton) == kMouseEventHandled);
-		EXPECT (b->isEditing () == true);
-		EXPECT (b->onMouseCancel() == kMouseEventHandled);
-		EXPECT (b->isEditing () == false);
+	p (10, 10);
+	EXPECT (b->onMouseDown (p, kLButton) == kMouseEventHandled);
+	EXPECT (b->isEditing () == true);
+	EXPECT (b->onMouseCancel () == kMouseEventHandled);
+	EXPECT (b->isEditing () == false);
 
-		EXPECT (b->onMouseMoved (p, kLButton) == kMouseEventNotHandled);
-	);
+	EXPECT (b->onMouseMoved (p, kLButton) == kMouseEventNotHandled);
+}
 
-	TEST(mouseEventsOnOffStyle,
-		auto b = owned (new CTextButton (CRect (10, 10, 50, 20)));
-		b->setStyle (CTextButton::kOnOffStyle);
-		b->setValue (b->getMin ());
-		CPoint p (10, 10);
-		EXPECT (b->onMouseDown (p, kRButton) == kMouseEventNotHandled);
-		EXPECT (b->isEditing () == false);
-		EXPECT (b->onMouseDown (p, kLButton) == kMouseEventHandled);
-		EXPECT (b->isEditing ());
-		EXPECT (b->getValue () == b->getMax ());
-		EXPECT (b->onMouseMoved (p, kLButton) == kMouseEventHandled);
-		EXPECT (b->isEditing ());
-		EXPECT (b->getValue () == b->getMax ());
-		EXPECT (b->onMouseUp (p, kLButton) == kMouseEventHandled);
-		EXPECT (b->isEditing () == false);
-		EXPECT (b->getValue () == b->getMax ());
-		
-		EXPECT (b->onMouseDown (p, kLButton) == kMouseEventHandled);
-		EXPECT (b->isEditing ());
-		EXPECT (b->onMouseUp (p, kLButton) == kMouseEventHandled);
-		EXPECT (b->isEditing () == false);
-		EXPECT (b->getValue () == b->getMin ());
+TEST_CASE (CTextButtonTest, MouseEventsOnOffStyle)
+{
+	auto b = owned (new CTextButton (CRect (10, 10, 50, 20)));
+	b->setStyle (CTextButton::kOnOffStyle);
+	b->setValue (b->getMin ());
+	CPoint p (10, 10);
+	EXPECT (b->onMouseDown (p, kRButton) == kMouseEventNotHandled);
+	EXPECT (b->isEditing () == false);
+	EXPECT (b->onMouseDown (p, kLButton) == kMouseEventHandled);
+	EXPECT (b->isEditing ());
+	EXPECT (b->getValue () == b->getMax ());
+	EXPECT (b->onMouseMoved (p, kLButton) == kMouseEventHandled);
+	EXPECT (b->isEditing ());
+	EXPECT (b->getValue () == b->getMax ());
+	EXPECT (b->onMouseUp (p, kLButton) == kMouseEventHandled);
+	EXPECT (b->isEditing () == false);
+	EXPECT (b->getValue () == b->getMax ());
 
-		EXPECT (b->onMouseDown (p, kLButton) == kMouseEventHandled);
-		EXPECT (b->isEditing ());
-		p (0, 0);
-		EXPECT (b->onMouseMoved (p, kLButton) == kMouseEventHandled);
-		EXPECT (b->getValue () == b->getMin ());
-		EXPECT (b->onMouseUp (p, kLButton) == kMouseEventHandled);
-		EXPECT (b->isEditing () == false);
-		EXPECT (b->getValue () == b->getMin ());
+	EXPECT (b->onMouseDown (p, kLButton) == kMouseEventHandled);
+	EXPECT (b->isEditing ());
+	EXPECT (b->onMouseUp (p, kLButton) == kMouseEventHandled);
+	EXPECT (b->isEditing () == false);
+	EXPECT (b->getValue () == b->getMin ());
 
-		p (10, 10);
-		EXPECT (b->onMouseDown (p, kLButton) == kMouseEventHandled);
-		EXPECT (b->isEditing () == true);
-		EXPECT (b->onMouseCancel() == kMouseEventHandled);
-		EXPECT (b->isEditing () == false);
+	EXPECT (b->onMouseDown (p, kLButton) == kMouseEventHandled);
+	EXPECT (b->isEditing ());
+	p (0, 0);
+	EXPECT (b->onMouseMoved (p, kLButton) == kMouseEventHandled);
+	EXPECT (b->getValue () == b->getMin ());
+	EXPECT (b->onMouseUp (p, kLButton) == kMouseEventHandled);
+	EXPECT (b->isEditing () == false);
+	EXPECT (b->getValue () == b->getMin ());
 
-		EXPECT (b->onMouseMoved (p, kLButton) == kMouseEventNotHandled);
-	);
+	p (10, 10);
+	EXPECT (b->onMouseDown (p, kLButton) == kMouseEventHandled);
+	EXPECT (b->isEditing () == true);
+	EXPECT (b->onMouseCancel () == kMouseEventHandled);
+	EXPECT (b->isEditing () == false);
 
-	TEST(keyEvents,
-		auto b = owned (new CTextButton (CRect (10, 10, 50, 20)));
-		b->setStyle (CTextButton::kOnOffStyle);
-		b->setValue (b->getMin());
-		VstKeyCode keyCode {};
-		keyCode.virt = VKEY_RETURN;
-		EXPECT (b->onKeyDown (keyCode) == 1);
-		EXPECT (b->getValue () == b->getMax ());
-		EXPECT (b->onKeyDown (keyCode) == 1);
-		EXPECT (b->getValue () == b->getMin ());
-		
-		keyCode.virt = 0;
-		keyCode.character = 't';
-		EXPECT (b->onKeyDown (keyCode) == -1);
+	EXPECT (b->onMouseMoved (p, kLButton) == kMouseEventNotHandled);
+}
 
-		b->setStyle (CTextButton::kKickStyle);
-		keyCode.character = 0;
-		keyCode.virt = VKEY_RETURN;
-		EXPECT (b->onKeyDown (keyCode) == 1);
-		EXPECT (b->getValue () == b->getMin ());
-		EXPECT (b->onKeyDown (keyCode) == 1);
-		EXPECT (b->getValue () == b->getMin ());
+TEST_CASE (CTextButtonTest, KeyEvents)
+{
+	auto b = owned (new CTextButton (CRect (10, 10, 50, 20)));
+	b->setStyle (CTextButton::kOnOffStyle);
+	b->setValue (b->getMin ());
+	VstKeyCode keyCode {};
+	keyCode.virt = VKEY_RETURN;
+	EXPECT (b->onKeyDown (keyCode) == 1);
+	EXPECT (b->getValue () == b->getMax ());
+	EXPECT (b->onKeyDown (keyCode) == 1);
+	EXPECT (b->getValue () == b->getMin ());
 
+	keyCode.virt = 0;
+	keyCode.character = 't';
+	EXPECT (b->onKeyDown (keyCode) == -1);
 
-		EXPECT (b->onKeyUp (keyCode) == -1);
-	);
-	
-	TEST(focusPathSetting,
-		auto b = owned (new CTextButton (CRect (10, 10, 50, 20)));
-		EXPECT (b->drawFocusOnTop () == false);
-	);
-);
+	b->setStyle (CTextButton::kKickStyle);
+	keyCode.character = 0;
+	keyCode.virt = VKEY_RETURN;
+	EXPECT (b->onKeyDown (keyCode) == 1);
+	EXPECT (b->getValue () == b->getMin ());
+	EXPECT (b->onKeyDown (keyCode) == 1);
+	EXPECT (b->getValue () == b->getMin ());
+
+	EXPECT (b->onKeyUp (keyCode) == -1);
+}
+
+TEST_CASE (CTextButtonTest, FocusPathSetting)
+{
+	auto b = owned (new CTextButton (CRect (10, 10, 50, 20)));
+	EXPECT (b->drawFocusOnTop () == false);
+}
 
 } // VSTGUI
--- vstgui/tests/unittest/lib/controls/cxypad_test.cpp
+++ vstgui/tests/unittest/lib/controls/cxypad_test.cpp
@@ -1,4 +1,4 @@
-// This file is part of VSTGUI. It is subject to the license terms 
+// This file is part of VSTGUI. It is subject to the license terms
 // in the LICENSE file found in the top-level directory of this
 // distribution and at http://github.com/steinbergmedia/vstgui/LICENSE
 
@@ -8,120 +8,122 @@
 
 namespace VSTGUI {
 
-TESTCASE(CXYPadTest,
+TEST_CASE (CXYPadTest, valueCalculation)
+{
+	auto value = CXYPad::calculateValue (0.f, 0.f);
+	float x = 1.f;
+	float y = 1.f;
+	CXYPad::calculateXY (value, x, y);
+	EXPECT (x == 0.f);
+	EXPECT (y == 0.f);
+	value = CXYPad::calculateValue (1.f, 0.f);
+	CXYPad::calculateXY (value, x, y);
+	EXPECT (x == 1.f);
+	EXPECT (y == 0.f);
+	value = CXYPad::calculateValue (0.f, 1.f);
+	CXYPad::calculateXY (value, x, y);
+	EXPECT (x == 0.f);
+	EXPECT (y == 1.f);
+	value = CXYPad::calculateValue (0.5f, 0.5f);
+	CXYPad::calculateXY (value, x, y);
+	EXPECT (x == 0.5f);
+	EXPECT (y == 0.5f);
+	value = CXYPad::calculateValue (0.25f, 0.25f);
+	CXYPad::calculateXY (value, x, y);
+	EXPECT (x == 0.25f);
+	EXPECT (y == 0.25f);
+}
 
-	TEST(valueCalculation,
-		auto value = CXYPad::calculateValue (0.f, 0.f);
-		float x = 1.f;
-		float y = 1.f;
-		CXYPad::calculateXY (value, x, y);
-		EXPECT (x == 0.f);
-		EXPECT (y == 0.f);
-		value = CXYPad::calculateValue (1.f, 0.f);
-		CXYPad::calculateXY (value, x, y);
-		EXPECT (x == 1.f);
-		EXPECT (y == 0.f);
-		value = CXYPad::calculateValue (0.f, 1.f);
-		CXYPad::calculateXY (value, x, y);
-		EXPECT (x == 0.f);
-		EXPECT (y == 1.f);
-		value = CXYPad::calculateValue (0.5f, 0.5f);
-		CXYPad::calculateXY (value, x, y);
-		EXPECT (x == 0.5f);
-		EXPECT (y == 0.5f);
-		value = CXYPad::calculateValue (0.25f, 0.25f);
-		CXYPad::calculateXY (value, x, y);
-		EXPECT (x == 0.25f);
-		EXPECT (y == 0.25f);
-	);
+TEST_CASE (CXYPadTest, MouseLeftDownInteraction)
+{
+	CXYPad pad (CRect (0, 0, 100, 100));
+	pad.setRoundRectRadius (0.f);
+	CPoint p (0, 0);
+	pad.onMouseDown (p, kLButton);
+	p (10, 10);
+	pad.onMouseMoved (p, kLButton);
+	float x = 1.f;
+	float y = 1.f;
+	pad.calculateXY (pad.getValue (), x, y);
+	EXPECT (x == 0.1f);
+	EXPECT (y == 0.1f);
+	p (110, 110);
+	pad.onMouseMoved (p, kLButton);
+	pad.calculateXY (pad.getValue (), x, y);
+	EXPECT (x == 1.f);
+	EXPECT (y == 1.f);
+	p (-10, -10);
+	pad.onMouseMoved (p, kLButton);
+	pad.calculateXY (pad.getValue (), x, y);
+	EXPECT (x == 0.f);
+	EXPECT (y == 0.f);
+	pad.onMouseUp (p, kLButton);
+}
 
-	TEST(mouseLeftDownInteraction,
-		CXYPad pad (CRect (0, 0, 100, 100));
-		pad.setRoundRectRadius (0.f);
-		CPoint p (0, 0);
-		pad.onMouseDown (p, kLButton);
-		p (10, 10);
-		pad.onMouseMoved (p, kLButton);
-		float x = 1.f;
-		float y = 1.f;
-		pad.calculateXY (pad.getValue (), x, y);
-		EXPECT(x == 0.1f);
-		EXPECT(y == 0.1f);
-		p (110, 110);
-		pad.onMouseMoved (p, kLButton);
-		pad.calculateXY (pad.getValue (), x, y);
-		EXPECT(x == 1.f);
-		EXPECT(y == 1.f);
-		p (-10, -10);
-		pad.onMouseMoved (p, kLButton);
-		pad.calculateXY (pad.getValue (), x, y);
-		EXPECT(x == 0.f);
-		EXPECT(y == 0.f);
-		pad.onMouseUp (p, kLButton);
-	);
+TEST_CASE (CXYPadTest, CancelMouseInteraction)
+{
+	CXYPad pad (CRect (0, 0, 100, 100));
+	float startX;
+	float startY;
+	pad.calculateXY (pad.getValue (), startX, startY);
+	CPoint p (0, 0);
+	pad.onMouseDown (p, kLButton);
+	p (10, 10);
+	pad.onMouseMoved (p, kLButton);
+	float x;
+	float y;
+	pad.calculateXY (pad.getValue (), x, y);
+	EXPECT (startX != x);
+	EXPECT (startY != y);
+	pad.onMouseCancel ();
+	pad.calculateXY (pad.getValue (), x, y);
+	EXPECT (startX == x);
+	EXPECT (startY == y);
+}
 
-	TEST(cancelMouseInteraction,
-		CXYPad pad (CRect (0, 0, 100, 100));
-		float startX;
-		float startY;
-		pad.calculateXY (pad.getValue (), startX, startY);
-		CPoint p (0, 0);
-		pad.onMouseDown (p, kLButton);
-		p (10, 10);
-		pad.onMouseMoved (p, kLButton);
-		float x;
-		float y;
-		pad.calculateXY (pad.getValue (), x, y);
-		EXPECT(startX != x);
-		EXPECT(startY != y);
-		pad.onMouseCancel ();
-		pad.calculateXY (pad.getValue (), x, y);
-		EXPECT(startX == x);
-		EXPECT(startY == y);
-	);
+TEST_CASE (CXYPadTest, OtherMouseInteraction)
+{
+	CXYPad pad (CRect (0, 0, 100, 100));
+	CPoint p (0, 0);
+	EXPECT (pad.onMouseDown (p, kRButton) == kMouseEventNotHandled);
+	EXPECT (pad.onMouseMoved (p, kRButton) == kMouseEventNotHandled);
+	EXPECT (pad.onMouseUp (p, kRButton) == kMouseEventNotHandled);
+}
 
-	TEST(otherMouseInteraction,
-		CXYPad pad (CRect (0, 0, 100, 100));
-		CPoint p (0, 0);
-		EXPECT (pad.onMouseDown (p, kRButton) == kMouseEventNotHandled);
-		EXPECT (pad.onMouseMoved (p, kRButton) == kMouseEventNotHandled);
-		EXPECT (pad.onMouseUp (p, kRButton) == kMouseEventNotHandled);
-	);
+TEST_CASE (CXYPadTest, StopTrackingOnMouseExit)
+{
+	CXYPad pad (CRect (0, 0, 100, 100));
+	pad.setStopTrackingOnMouseExit (true);
+	pad.setRoundRectRadius (0.f);
+	CPoint p (0, 0);
+	EXPECT (pad.onMouseDown (p, kLButton) == kMouseEventHandled);
+	EXPECT (pad.isEditing ());
+	p (50, 50);
+	EXPECT (pad.onMouseMoved (p, kLButton) == kMouseEventHandled);
+	EXPECT (pad.isEditing ());
+	p (150, 150);
+	EXPECT (pad.onMouseMoved (p, kLButton) == kMouseMoveEventHandledButDontNeedMoreEvents);
+	EXPECT (pad.isEditing () == false);
+}
 
-	TEST(stopTrackingOnMouseExit,
-		CXYPad pad (CRect (0, 0, 100, 100));
-		pad.setStopTrackingOnMouseExit (true);
-		pad.setRoundRectRadius (0.f);
-		CPoint p (0, 0);
-		EXPECT (pad.onMouseDown (p, kLButton) == kMouseEventHandled);
-		EXPECT (pad.isEditing ());
-		p (50, 50);
-		EXPECT (pad.onMouseMoved (p, kLButton) == kMouseEventHandled);
-		EXPECT (pad.isEditing ());
-		p (150, 150);
-		EXPECT (pad.onMouseMoved (p, kLButton) == kMouseMoveEventHandledButDontNeedMoreEvents);
-		EXPECT (pad.isEditing () == false);
-	);
-	
-	TEST(mouseWheel,
-		CXYPad pad (CRect (0, 0, 100, 100));
-		float x = 1.f;
-		float y = 1.f;
-		CXYPad::calculateXY (pad.getValue (), x, y);
-		EXPECT(x == 0.f && y == 0.f);
-		MouseWheelEvent event;
-		event.mousePosition = CPoint (1, 1);
-		event.deltaX = 1.;
-		pad.onMouseWheelEvent (event);
-		CXYPad::calculateXY (pad.getValue (), x, y);
-		EXPECT(x == pad.getWheelInc () && y == 0.f);
-		event.deltaX = 0.;
-		event.deltaY = 1.;
-		pad.onMouseWheelEvent (event);
-		CXYPad::calculateXY (pad.getValue (), x, y);
-		EXPECT(x == pad.getWheelInc () && y == pad.getWheelInc ());
-
-	);
-);
+TEST_CASE (CXYPadTest, MouseWheel)
+{
+	CXYPad pad (CRect (0, 0, 100, 100));
+	float x = 1.f;
+	float y = 1.f;
+	CXYPad::calculateXY (pad.getValue (), x, y);
+	EXPECT (x == 0.f && y == 0.f);
+	MouseWheelEvent event;
+	event.mousePosition = CPoint (1, 1);
+	event.deltaX = 1.;
+	pad.onMouseWheelEvent (event);
+	CXYPad::calculateXY (pad.getValue (), x, y);
+	EXPECT (x == pad.getWheelInc () && y == 0.f);
+	event.deltaX = 0.;
+	event.deltaY = 1.;
+	pad.onMouseWheelEvent (event);
+	CXYPad::calculateXY (pad.getValue (), x, y);
+	EXPECT (x == pad.getWheelInc () && y == pad.getWheelInc ());
+}
 
 } // VSTGUI
