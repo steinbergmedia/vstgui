d4c20289c49cb2aba64264d41998091f6651c02d
scheffle <scheffle@users.noreply.github.com>
2021-06-09 16:21:44 +0200

    fix wrong clip rect calculation on linux
    The correctly transformed clip rect is already in the current state. Applying the current transform may add other transforms that were set after the clip was set.

--- vstgui/lib/platform/linux/cairocontext.h
+++ vstgui/lib/platform/linux/cairocontext.h
@@ -58,6 +58,7 @@ public:
 	void beginDraw () override;
 	void endDraw () override;
 
+	CRect getCurrentStateClipRect () const;
 private:
 	void init () override;
 	void setSourceColor (CColor color);
