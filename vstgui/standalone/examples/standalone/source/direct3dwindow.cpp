fe218921de59fee622be9b9876feacb473bb8bc6
scheffle <scheffle@users.noreply.github.com>
2022-10-06 17:36:25 +0200

    minimize code

--- vstgui/standalone/examples/standalone/source/direct3dwindow.cpp
+++ vstgui/standalone/examples/standalone/source/direct3dwindow.cpp
@@ -55,31 +55,6 @@ inline constexpr UINT D3D12CalcSubresource (UINT MipSlice, UINT ArraySlice, UINT
 	return MipSlice + ArraySlice * MipLevels + PlaneSlice * MipLevels * ArraySize;
 }
 
-struct CD3DX12_BLEND_DESC : public D3D12_BLEND_DESC
-{
-	CD3DX12_BLEND_DESC () = default;
-	explicit CD3DX12_BLEND_DESC (const D3D12_BLEND_DESC& o) noexcept : D3D12_BLEND_DESC (o) {}
-	explicit CD3DX12_BLEND_DESC (CD3DX12_DEFAULT) noexcept
-	{
-		AlphaToCoverageEnable = FALSE;
-		IndependentBlendEnable = FALSE;
-		const D3D12_RENDER_TARGET_BLEND_DESC defaultRenderTargetBlendDesc = {
-			FALSE,
-			FALSE,
-			D3D12_BLEND_ONE,
-			D3D12_BLEND_ZERO,
-			D3D12_BLEND_OP_ADD,
-			D3D12_BLEND_ONE,
-			D3D12_BLEND_ZERO,
-			D3D12_BLEND_OP_ADD,
-			D3D12_LOGIC_OP_NOOP,
-			D3D12_COLOR_WRITE_ENABLE_ALL,
-		};
-		for (UINT i = 0; i < D3D12_SIMULTANEOUS_RENDER_TARGET_COUNT; ++i)
-			RenderTarget[i] = defaultRenderTargetBlendDesc;
-	}
-};
-
 struct CD3DX12_HEAP_PROPERTIES : public D3D12_HEAP_PROPERTIES
 {
 	CD3DX12_HEAP_PROPERTIES () = default;
@@ -384,6 +359,7 @@ struct ExampleRenderer : public ExternalView::IDirect3D12Renderer
 		timer = nullptr;
 #endif
 		freeFrameResources ();
+		onDestroy ();
 	}
 
 	uint32_t getFrameCount () const override { return FrameCount; }
@@ -489,7 +465,13 @@ struct ExampleRenderer : public ExternalView::IDirect3D12Renderer
 									   FALSE,
 									   0,
 									   D3D12_CONSERVATIVE_RASTERIZATION_MODE_OFF};
-			psoDesc.BlendState = CD3DX12_BLEND_DESC (D3D12_DEFAULT);
+			constexpr D3D12_BLEND_DESC DefaultBlendDesc = {
+				FALSE,
+				FALSE,
+				{FALSE, FALSE, D3D12_BLEND_ONE, D3D12_BLEND_ZERO, D3D12_BLEND_OP_ADD,
+				 D3D12_BLEND_ONE, D3D12_BLEND_ZERO, D3D12_BLEND_OP_ADD, D3D12_LOGIC_OP_NOOP,
+				 D3D12_COLOR_WRITE_ENABLE_ALL}};
+			psoDesc.BlendState = DefaultBlendDesc;
 			psoDesc.DepthStencilState.DepthEnable = FALSE;
 			psoDesc.DepthStencilState.StencilEnable = FALSE;
 			psoDesc.SampleMask = UINT_MAX;
