2f68399b7ab0add788d5ddf698234525a647e34f
scheffle <scheffle@users.noreply.github.com>
2023-10-24 11:58:36 +0200

    [update] version patchlevel

--- vstgui/lib/vstguibase.h
+++ vstgui/lib/vstguibase.h
@@ -13,7 +13,7 @@
 //-----------------------------------------------------------------------------
 #define VSTGUI_VERSION_MAJOR  4
 #define VSTGUI_VERSION_MINOR  13
-#define VSTGUI_VERSION_PATCHLEVEL  0
+#define VSTGUI_VERSION_PATCHLEVEL  1
 
 //-----------------------------------------------------------------------------
 // Platform definitions
