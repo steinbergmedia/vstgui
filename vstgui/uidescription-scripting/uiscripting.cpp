8b7dbb81e5b61ad9ab5662f5e32c5afbfb4a2aff
scheffle <scheffle@users.noreply.github.com>
2024-01-21 17:55:55 +0100

    more drawing via script

--- vstgui/uidescription-scripting/uiscripting.cpp
+++ vstgui/uidescription-scripting/uiscripting.cpp
@@ -1224,6 +1224,7 @@ void ViewScriptObject::onDestroy (CScriptVar* v)
 //------------------------------------------------------------------------
 DrawContextObject::DrawContextObject ()
 {
+	// TODO: decide if this object should expose the same interface as "CanvasRenderingContext2D"
 	scriptVar->setLifeTimeObserver (this);
 	addFunc ("drawLine"sv,
 			 [this] (CScriptVar* var) {
@@ -1265,6 +1266,29 @@ DrawContextObject::DrawContextObject ()
 				 context->drawRect (rect, style);
 			 },
 			 {"rect", "style"});
+	addFunc ("drawEllipse"sv,
+			 [this] (CScriptVar* var) {
+				 if (!context)
+					 throw CScriptException ("Native context is missing!");
+				 auto rectVar = var->getParameter ("rect"sv);
+				 if (!rectVar)
+					 throw CScriptException (
+						 "Missing `rect` argument in drawContext.drawEllipse(rect, style);");
+				 auto styleVar = var->getParameter ("style"sv);
+				 if (!styleVar)
+					 throw CScriptException (
+						 "Missing `style` argument in drawContext.drawEllipse(rect, style);");
+				 auto rect = fromScriptRect (*rectVar);
+				 CDrawStyle style {};
+				 if (styleVar->getString () == "stroked")
+					 style = kDrawStroked;
+				 else if (styleVar->getString () == "filled")
+					 style = kDrawFilled;
+				 else if (styleVar->getString () == "filledAndStroked")
+					 style = kDrawFilledAndStroked;
+				 context->drawEllipse (rect, style);
+			 },
+			 {"rect", "style"});
 	addFunc ("clearRect"sv,
 			 [this] (CScriptVar* var) {
 				 if (!context)
@@ -1277,6 +1301,136 @@ DrawContextObject::DrawContextObject ()
 				 context->clearRect (rect);
 			 },
 			 {"rect", "style"});
+	addFunc ("drawPolygon"sv,
+			 [this] (CScriptVar* var) {
+				 if (!context)
+					 throw CScriptException ("Native context is missing!");
+				 auto pointsVar = var->getParameter ("points"sv);
+				 if (!pointsVar)
+					 throw CScriptException (
+						 "Missing `points` argument in drawContext.drawPolygon(points, style);");
+				 if (!pointsVar->isArray ())
+					 throw CScriptException ("`points` argument must be an array of points in "
+											 "drawContext.drawPolygon(points, style);");
+				 auto styleVar = var->getParameter ("style"sv);
+				 if (!styleVar)
+					 throw CScriptException (
+						 "Missing `style` argument in drawContext.drawPolygon(points, style);");
+				 PointList points;
+				 auto numPoints = pointsVar->getArrayLength ();
+				 for (auto index = 0; index < numPoints; ++index)
+				 {
+					 auto pointVar = pointsVar->getArrayIndex (index);
+					 vstgui_assert (pointVar != nullptr);
+					 points.emplace_back (fromScriptPoint (*pointVar));
+				 }
+				 CDrawStyle style {};
+				 if (styleVar->getString () == "stroked")
+					 style = kDrawStroked;
+				 else if (styleVar->getString () == "filled")
+					 style = kDrawFilled;
+				 else if (styleVar->getString () == "filledAndStroked")
+					 style = kDrawFilledAndStroked;
+				 context->drawPolygon (points, style);
+			 },
+			 {"points", "style"});
+	addFunc ("setClipRect"sv,
+			 [this] (CScriptVar* var) {
+				 if (!context)
+					 throw CScriptException ("Native context is missing!");
+				 auto rectVar = var->getParameter ("rect"sv);
+				 if (!rectVar)
+					 throw CScriptException (
+						 "Missing `rect` argument in drawContext.setClipRect(rect);");
+				 auto rect = fromScriptRect (*rectVar);
+				 context->setClipRect (rect);
+			 },
+			 {"rect"});
+#if 1 // TODO: make a bitmap js object instead
+	addFunc ("drawBitmap"sv,
+			 [this] (CScriptVar* var) {
+				 if (!context)
+					 throw CScriptException ("Native context is missing!");
+				 auto bitmapNameVar = var->getParameter ("bitmap"sv);
+				 if (!bitmapNameVar)
+					 throw CScriptException (
+						 "Missing `bitmap` argument in drawContext.drawBitmap(bitmap, destRect, "
+						 "offsetPoint, alpha);");
+				 auto destRectVar = var->getParameter ("destRect"sv);
+				 if (!destRectVar)
+					 throw CScriptException (
+						 "Missing `destRect` argument in drawContext.drawBitmap(bitmap, destRect, "
+						 "offsetPoint, alpha);");
+				 auto offsetPointVar = var->findChild ("offsetPoint"sv);
+				 auto alphaVar = var->findChild ("alpha"sv);
+				 auto bitmap = uiDesc->getBitmap (bitmapNameVar->getString ().data ());
+				 if (!bitmap)
+					 throw CScriptException ("bitmap not found in uiDescription");
+				 auto destRect = fromScriptRect (*destRectVar);
+				 auto offset =
+					 offsetPointVar ? fromScriptPoint (*offsetPointVar->getVar ()) : CPoint (0, 0);
+				 auto alpha = alphaVar ? alphaVar->getVar ()->getDouble () : 1.f;
+				 context->drawBitmap (bitmap, destRect, offset, alpha);
+			 },
+			 {"bitmap", "destRect"});
+#endif
+	addFunc (
+		"drawString"sv,
+		[this] (CScriptVar* var) {
+			if (!context)
+				throw CScriptException ("Native context is missing!");
+			auto stringVar = var->getParameter ("string"sv);
+			if (!stringVar)
+				throw CScriptException (
+					"Missing `string` argument in drawContext.drawString(string, rect, align);");
+			auto rectVar = var->getParameter ("rect"sv);
+			if (!rectVar)
+				throw CScriptException (
+					"Missing `rect` argument in drawContext.drawString(string, rect, align);");
+			auto alignVar = var->getParameter ("align"sv);
+			if (!alignVar)
+				throw CScriptException (
+					"Missing `align` argument in drawContext.drawString(string, rect, align);");
+			auto string = stringVar->getString ().data ();
+			auto rect = fromScriptRect (*rectVar);
+			CHoriTxtAlign align = kCenterText;
+			if (alignVar->getString () == "left")
+				align = kLeftText;
+			else if (alignVar->getString () == "center")
+				align = kCenterText;
+			else if (alignVar->getString () == "right")
+				align = kRightText;
+			else
+				throw CScriptException (
+					"wrong `align` argument. expected 'left', 'center' or 'right'");
+			context->drawString (string, rect, align, true);
+		},
+		{"string", "rect", "align"});
+	addFunc ("setFont"sv,
+			 [this] (CScriptVar* var) {
+				 if (!context)
+					 throw CScriptException ("Native context is missing!");
+				 auto fontVar = var->getParameter ("font"sv);
+				 if (!fontVar)
+					 throw CScriptException (
+						 "Missing `font` argument in drawContext.setFont(font);");
+				 if (auto font = uiDesc->getFont (fontVar->getString ().data ()))
+					 context->setFont (font);
+			 },
+			 {"font"});
+	addFunc ("setFontColor"sv,
+			 [this] (CScriptVar* var) {
+				 if (!context)
+					 throw CScriptException ("Native context is missing!");
+				 auto colorVar = var->getParameter ("color"sv);
+				 if (!colorVar)
+					 throw CScriptException (
+						 "Missing `color` argument in drawContext.setFontColor(color);");
+				 CColor color {};
+				 UIViewCreator::stringToColor (&colorVar->getString (), color, uiDesc);
+				 context->setFontColor (color);
+			 },
+			 {"color"});
 	addFunc ("setFillColor"sv,
 			 [this] (CScriptVar* var) {
 				 if (!context)
@@ -1316,32 +1470,6 @@ DrawContextObject::DrawContextObject ()
 				 context->setLineWidth (widthVar->getDouble ());
 			 },
 			 {"width"});
-	addFunc ("drawBitmap"sv,
-			 [this] (CScriptVar* var) {
-				 if (!context)
-					 throw CScriptException ("Native context is missing!");
-				 auto bitmapNameVar = var->getParameter ("bitmap"sv);
-				 if (!bitmapNameVar)
-					 throw CScriptException (
-						 "Missing `bitmap` argument in drawContext.drawBitmap(bitmap, destRect, "
-						 "offsetPoint, alpha);");
-				 auto destRectVar = var->getParameter ("destRect"sv);
-				 if (!destRectVar)
-					 throw CScriptException (
-						 "Missing `destRect` argument in drawContext.drawBitmap(bitmap, destRect, "
-						 "offsetPoint, alpha);");
-				 auto offsetPointVar = var->findChild ("offsetPoint"sv);
-				 auto alphaVar = var->findChild ("alpha"sv);
-				 auto bitmap = uiDesc->getBitmap (bitmapNameVar->getString ().data ());
-				 if (!bitmap)
-					 throw CScriptException ("bitmap not found in uiDescription");
-				 auto destRect = fromScriptRect (*destRectVar);
-				 auto offset =
-					 offsetPointVar ? fromScriptPoint (*offsetPointVar->getVar ()) : CPoint (0, 0);
-				 auto alpha = alphaVar ? alphaVar->getVar ()->getDouble () : 1.f;
-				 context->drawBitmap (bitmap, destRect, offset, alpha);
-			 },
-			 {"bitmap", "destRect"});
 }
 
 //------------------------------------------------------------------------
@@ -1368,6 +1496,8 @@ void JavaScriptDrawableView::drawRect (CDrawContext* context, const CRect& rect)
 	auto scriptContext = scriptObject->getContext ();
 	if (!scriptContext)
 		return;
+	context->saveGlobalState ();
+
 	drawContext.setDrawContext (context, scriptContext->getUIDescription ());
 
 	CDrawContext::Transform tm (*context,
@@ -1381,6 +1511,8 @@ void JavaScriptDrawableView::drawRect (CDrawContext* context, const CRect& rect)
 	scriptContext->evalScript ("view.draw(context, rect);"sv);
 
 	drawContext.setDrawContext (nullptr, nullptr);
+
+	context->restoreGlobalState ();
 }
 
 //------------------------------------------------------------------------
