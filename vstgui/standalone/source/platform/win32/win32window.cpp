2cc87b4cd96b20a79645ceb137bf25630bc0220a
Arne Scheffler <a.scheffler@steinberg.de>
2024-02-16 10:44:18 +0100

    forward declare stuff for older Win SDKs

--- vstgui/standalone/source/platform/win32/win32window.cpp
+++ vstgui/standalone/source/platform/win32/win32window.cpp
@@ -24,6 +24,21 @@
 #pragma comment(lib, "Dwmapi.lib")
 #pragma comment(lib, "Comctl32.lib")
 
+#if NTDDI_VERSION < 0x0A00000C
+
+#define DWMWA_USE_IMMERSIVE_DARK_MODE 20
+#define DWMWA_SYSTEMBACKDROP_TYPE 38
+enum DWM_SYSTEMBACKDROP_TYPE
+{
+    DWMSBT_AUTO,             // [Default] Let DWM automatically decide the system-drawn backdrop for this window.
+    DWMSBT_NONE,             // Do not draw any system backdrop.
+    DWMSBT_MAINWINDOW,       // Draw the backdrop material effect corresponding to a long-lived window.
+    DWMSBT_TRANSIENTWINDOW,  // Draw the backdrop material effect corresponding to a transient window.
+    DWMSBT_TABBEDWINDOW,     // Draw the backdrop material effect corresponding to a window with a tabbed title bar.
+};
+
+#endif
+
 //------------------------------------------------------------------------
 namespace VSTGUI {
 namespace Standalone {
