1267e8f77a463e9f4e25ff89c70d4aa4614d1b7b
rehans <rene@hansen-audio.de>
2025-09-26 11:38:48 +0200

    Correct license header

--- vstgui/lib/platform/linux/waylandclientcontext.cpp
+++ vstgui/lib/platform/linux/waylandclientcontext.cpp
@@ -1,39 +1,6 @@
-//-----------------------------------------------------------------------------
-// Flags       : clang-format auto
-// Project     : VST SDK
-//
-// Category    : EditorHost
-// Filename    : public.sdk/samples/vst-hosting/editorhost/source/platform/linux/client.cpp
-// Created by  : Steinberg 05.2025
-// Description : Implemenation of WaylandServerDelegate::IWaylandClientContext
-//
-//-----------------------------------------------------------------------------
-// LICENSE
-// (c) 2023, Steinberg Media Technologies GmbH, All Rights Reserved
-//-----------------------------------------------------------------------------
-// Redistribution and use in source and binary forms, with or without modification,
-// are permitted provided that the following conditions are met:
-//
-//   * Redistributions of source code must retain the above copyright notice,
-//     this list of conditions and the following disclaimer.
-//   * Redistributions in binary form must reproduce the above copyright notice,
-//     this list of conditions and the following disclaimer in the documentation
-//     and/or other materials provided with the distribution.
-//   * Neither the name of the Steinberg Media Technologies nor the names of its
-//     contributors may be used to endorse or promote products derived from this
-//     software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
-// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
-// OF THE POSSIBILITY OF SUCH DAMAGE.
-//-----------------------------------------------------------------------------
+// This file is part of VSTGUI. It is subject to the license terms
+// in the LICENSE file found in the top-level directory of this
+// distribution and at http://github.com/steinbergmedia/vstgui/LICENSE
 
 #include "waylandclientcontext.h"
 
@@ -493,9 +460,9 @@ int WaylandClientContext::countOutputs () const
 }
 
 //------------------------------------------------------------------------
-zwp_linux_dmabuf_v1* WaylandClientContext::getDmaBuffer () const 
-{ 
-	return impl->globals.dmabuf; 
+zwp_linux_dmabuf_v1* WaylandClientContext::getDmaBuffer () const
+{
+	return impl->globals.dmabuf;
 }
 
 //------------------------------------------------------------------------
@@ -509,4 +476,4 @@ const WaylandOutput& WaylandClientContext::getOutput (int index) const
 }
 
 //------------------------------------------------------------------------
-} // namespace VSTGUI::Wayland 
+} // namespace VSTGUI::Wayland
