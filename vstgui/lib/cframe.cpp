steinbergmedia/vstgui

commit 485827ff89af7664f192940f56b619fa46f5c91e

* support drawing the best CBitmap representation according to the scale factor in the D2D backend 	* support setting a transform matrix on CFrame directly
+ add zoom example to the uidescription test

vstgui/lib/cframe.cpp
@@ -450,13 +450,16 @@ void CFrame::checkMouseViews (const CPoint& where, const CButtonState& buttons)
 //-----------------------------------------------------------------------------
 CMouseEventResult CFrame::onMouseDown (CPoint &where, const CButtonState& buttons)
 {
+	CPoint where2 (where);
+	getTransform ().inverse ().transform (where2);
+
 	// reset views
 	mouseDownView = 0;
 	if (pFocusView && dynamic_cast<CTextEdit*> (pFocusView))
 		setFocusView (0);
 
 	if (pTooltips)
-		pTooltips->onMouseDown (where);
+		pTooltips->onMouseDown (where2);
 
 	CMouseEventResult result = callMouseObserverMouseDown (where, buttons);
 	if (result != kMouseEventNotHandled)
@@ -466,9 +469,9 @@ CMouseEventResult CFrame::onMouseDown (CPoint &where, const CButtonState& button
 	{
 		CBaseObjectGuard rg (pModalView);
 
-		if (pModalView->isVisible () && pModalView->getMouseEnabled () && pModalView->hitTest (where, buttons))
+		if (pModalView->isVisible () && pModalView->getMouseEnabled () && pModalView->hitTest (where2, buttons))
 		{
-			CMouseEventResult result = pModalView->onMouseDown (where, buttons);
+			CMouseEventResult result = pModalView->onMouseDown (where2, buttons);
 			if (result == kMouseEventHandled)
 			{
 				mouseDownView = pModalView;
@@ -493,8 +496,11 @@ CMouseEventResult CFrame::onMouseUp (CPoint &where, const CButtonState& buttons)
 //-----------------------------------------------------------------------------
 CMouseEventResult CFrame::onMouseMoved (CPoint &where, const CButtonState& buttons)
 {
+	CPoint where2 (where);
+	getTransform ().inverse ().transform (where2);
+	
 	if (pTooltips)
-		pTooltips->onMouseMoved (where);
+		pTooltips->onMouseMoved (where2);
 
 	checkMouseViews (where, buttons);
 
@@ -505,7 +511,7 @@ CMouseEventResult CFrame::onMouseMoved (CPoint &where, const CButtonState& butto
 	if (pModalView)
 	{
 		CBaseObjectGuard rg (pModalView);
-		result = pModalView->onMouseMoved (where, buttons);
+		result = pModalView->onMouseMoved (where2, buttons);
 	}
 	else
 	{
@@ -860,7 +866,13 @@ void CFrame::endEdit (int32_t index)
 bool CFrame::getCurrentMouseLocation (CPoint &where) const
 {
 	if (platformFrame)
-		return platformFrame->getCurrentMousePosition (where);
+	{
+		if (platformFrame->getCurrentMousePosition (where))
+		{
+			getTransform().transform (where);
+			return true;
+		}
+	}
 	return false;
 }
 
@@ -1075,14 +1087,16 @@ CView* CFrame::getViewAt (const CPoint& where, bool deep, bool mustbeMouseEnable
 {
 	if (pModalView)
 	{
-		if (where.isInside (pModalView->getViewSize ()))
+		CPoint where2 (where);
+		getTransform ().inverse ().transform (where2);
+		if (where2.isInside (pModalView->getViewSize ()))
 		{
 			if (deep)
 			{
 				CViewContainer* container = dynamic_cast<CViewContainer*> (pModalView);
 				if (container)
 				{
-					return container->getViewAt (where, deep, mustbeMouseEnabled);
+					return container->getViewAt (where2, deep, mustbeMouseEnabled);
 				}
 			}
 			return pModalView;
@@ -1097,13 +1111,15 @@ CViewContainer* CFrame::getContainerAt (const CPoint& where, bool deep, bool mus
 {
 	if (pModalView)
 	{
-		if (where.isInside (pModalView->getViewSize ()))
+		CPoint where2 (where);
+		getTransform ().inverse ().transform (where2);
+		if (where2.isInside (pModalView->getViewSize ()))
 		{
 			CViewContainer* container = dynamic_cast<CViewContainer*> (pModalView);
 			if (container)
 			{
 				if (deep)
-					return container->getContainerAt (where, deep, mustbeMouseEnabled);
+					return container->getContainerAt (where2, deep, mustbeMouseEnabled);
 				return container;
 			}
 		}
@@ -1222,10 +1238,12 @@ void CFrame::invalidate (const CRect &rect)
 //-----------------------------------------------------------------------------
 void CFrame::invalidRect (const CRect& rect)
 {
-	if (!isVisible ())
+	if (!isVisible () || !platformFrame)
 		return;
-	if (platformFrame)
-		platformFrame->invalidRect (rect);
+
+	CRect _rect (rect);
+	getTransform ().transform (_rect);
+	platformFrame->invalidRect (_rect);
 }
 
 //-----------------------------------------------------------------------------
@@ -1353,11 +1371,14 @@ void CFrame::callMouseObserverMouseExited (CView* view)
 }
 
 //-----------------------------------------------------------------------------
-CMouseEventResult CFrame::callMouseObserverMouseDown (const CPoint& where, const CButtonState& buttons)
+CMouseEventResult CFrame::callMouseObserverMouseDown (const CPoint& _where, const CButtonState& buttons)
 {
 	CMouseEventResult result = kMouseEventNotHandled;
 	if (pMouseObservers)
 	{
+		CPoint where (_where);
+		getTransform ().inverse ().transform (where);
+		
 		for (MouseObserverList::const_iterator it = pMouseObservers->begin (), end = pMouseObservers->end (); it != end; it++)
 		{
 			CMouseEventResult result2 = (*it)->onMouseDown (this, where, buttons);
@@ -1371,11 +1392,14 @@ CMouseEventResult CFrame::callMouseObserverMouseDown (const CPoint& where, const
 }
 
 //-----------------------------------------------------------------------------
-CMouseEventResult CFrame::callMouseObserverMouseMoved (const CPoint& where, const CButtonState& buttons)
+CMouseEventResult CFrame::callMouseObserverMouseMoved (const CPoint& _where, const CButtonState& buttons)
 {
 	CMouseEventResult result = kMouseEventNotHandled;
 	if (pMouseObservers)
 	{
+		CPoint where (_where);
+		getTransform ().inverse ().transform (where);
+		
 		for (MouseObserverList::const_iterator it = pMouseObservers->begin (), end = pMouseObservers->end (); it != end; it++)
 		{
 			CMouseEventResult result2 = (*it)->onMouseMoved (this, where, buttons);

vstgui/lib/controls/ctextedit.cpp
@@ -255,6 +255,19 @@ int32_t CTextEdit::onKeyDown (VstKeyCode& keyCode)
 }
 
 //------------------------------------------------------------------------
+CFontRef CTextEdit::platformGetFont () const
+{
+	CFontRef font = getFont ();
+	CCoord fontSize = font->getSize ();
+	fontSize *= getGlobalTransform ().m11;
+	if (fontSize == font->getSize ())
+		return font;
+	platformFont = owned (new CFontDesc (*font));
+	platformFont->setSize (fontSize);
+	return platformFont;
+}
+
+//------------------------------------------------------------------------
 CRect CTextEdit::platformGetSize () const
 {
 	return translateToGlobal (getViewSize ());

vstgui/lib/controls/ctextedit.h
@@ -108,7 +108,7 @@ protected:
 
 	CColor platformGetBackColor () const VSTGUI_OVERRIDE_VMETHOD { return getBackColor (); }
 	CColor platformGetFontColor () const VSTGUI_OVERRIDE_VMETHOD { return getFontColor (); }
-	CFontRef platformGetFont () const VSTGUI_OVERRIDE_VMETHOD { return getFont (); }
+	CFontRef platformGetFont () const VSTGUI_OVERRIDE_VMETHOD;
 	CHoriTxtAlign platformGetHoriTxtAlign () const VSTGUI_OVERRIDE_VMETHOD { return getHoriAlign (); }
 	UTF8StringPtr platformGetText () const VSTGUI_OVERRIDE_VMETHOD { return text; }
 	CRect platformGetSize () const VSTGUI_OVERRIDE_VMETHOD;
@@ -128,6 +128,7 @@ protected:
 #endif
 
 	bool immediateTextChange;
+	mutable SharedPointer<CFontDesc> platformFont;
 };
 
 } // namespace

vstgui/lib/cviewcontainer.cpp
@@ -155,6 +155,7 @@ void CViewContainer::setViewSize (const CRect &rect, bool invalid)
 	{
 		CCoord widthDelta = rect.getWidth () - oldSize.getWidth ();
 		CCoord heightDelta = rect.getHeight () - oldSize.getHeight ();
+		getTransform ().inverse ().transform (widthDelta, heightDelta);
 
 		if (widthDelta != 0 || heightDelta != 0)
 		{

vstgui/lib/cvstguitimer.h
@@ -103,7 +103,7 @@ namespace Call
 	typedef std::function<void ()> FunctionCallback;
 
 	/** Trigger a function call at a later timer */
-	static void later (FunctionCallback callback, int32_t delayInMilliseconds = 10)
+	inline void later (FunctionCallback callback, int32_t delayInMilliseconds = 10)
 	{
 		new CVSTGUITimer ([callback] (CVSTGUITimer* timer) {
 			callback ();

vstgui/lib/platform/win32/direct2d/d2dbitmap.cpp
@@ -60,6 +60,7 @@ protected:
 //-----------------------------------------------------------------------------
 D2DBitmap::D2DBitmap ()
 : source (0)
+, scaleFactor (1.)
 {
 }
 
@@ -67,6 +68,7 @@ D2DBitmap::D2DBitmap ()
 D2DBitmap::D2DBitmap (const CPoint& size)
 : source (0)
 , size (size)
+, scaleFactor (1.)
 {
 	REFWICPixelFormatGUID pixelFormat = GUID_WICPixelFormat32bppPBGRA;
 	WICBitmapCreateCacheOption options = WICBitmapCacheOnLoad;

vstgui/lib/platform/win32/direct2d/d2dbitmap.h
@@ -60,8 +60,8 @@ public:
 	bool load (const CResourceDescription& desc) VSTGUI_OVERRIDE_VMETHOD;
 	const CPoint& getSize () const VSTGUI_OVERRIDE_VMETHOD { return size; }
 	IPlatformBitmapPixelAccess* lockPixels (bool alphaPremultiplied) VSTGUI_OVERRIDE_VMETHOD;
-	void setScaleFactor (double factor) VSTGUI_OVERRIDE_VMETHOD {}
-	double getScaleFactor () const VSTGUI_OVERRIDE_VMETHOD { return 1.; }
+	void setScaleFactor (double factor) VSTGUI_OVERRIDE_VMETHOD { scaleFactor = factor; }
+	double getScaleFactor () const VSTGUI_OVERRIDE_VMETHOD { return scaleFactor; }
 
 	HBITMAP createHBitmap ();
 	bool loadFromStream (IStream* stream);
@@ -97,6 +97,7 @@ protected:
 	};
 
 	CPoint size;
+	double scaleFactor;
 	IWICBitmapSource* source;
 };
 

vstgui/lib/platform/win32/direct2d/d2ddrawcontext.cpp
@@ -403,7 +403,12 @@ void D2DDrawContext::clearRect (const CRect& rect)
 //-----------------------------------------------------------------------------
 void D2DDrawContext::drawBitmap (CBitmap* bitmap, const CRect& dest, const CPoint& offset, float alpha)
 {
-	D2DBitmap* d2dBitmap = bitmap->getPlatformBitmap () ? dynamic_cast<D2DBitmap*> (bitmap->getPlatformBitmap ()) : 0;
+	double transformedScaleFactor = getScaleFactor ();
+	CGraphicsTransform t = getCurrentTransform ();
+	if (t.m11 == t.m22 && t.m12 == 0 && t.m21 == 0)
+		transformedScaleFactor *= t.m11;
+	IPlatformBitmap* platformBitmap = bitmap->getBestPlatformBitmapForScaleFactor (transformedScaleFactor);
+	D2DBitmap* d2dBitmap = platformBitmap ? dynamic_cast<D2DBitmap*> (platformBitmap) : 0;
 	if (renderTarget && d2dBitmap)
 	{
 		if (d2dBitmap->getSource ())
@@ -411,12 +416,21 @@ void D2DDrawContext::drawBitmap (CBitmap* bitmap, const CRect& dest, const CPoin
 			ID2D1Bitmap* d2d1Bitmap = D2DBitmapCache::instance ()->getBitmap (d2dBitmap, renderTarget);
 			if (d2d1Bitmap)
 			{
+				double bitmapScaleFactor = platformBitmap->getScaleFactor ();
+				CGraphicsTransform bitmapTransform;
+				bitmapTransform.scale (bitmapScaleFactor, bitmapScaleFactor);
+				Transform transform (*this, bitmapTransform.inverse ());
+
 				D2DApplyClip clip (this);
 				CRect d (dest);
 				d.makeIntegral ();
 				CRect source (dest);
 				source.offset (-source.left, -source.top);
 				source.offset (offset.x, offset.y);
+
+				bitmapTransform.transform (source);
+				bitmapTransform.transform (d);
+
 				D2D1_RECT_F sourceRect = makeD2DRect (source);
 				renderTarget->DrawBitmap (d2d1Bitmap, makeD2DRect (d), alpha * currentState.globalAlpha, D2D1_BITMAP_INTERPOLATION_MODE_LINEAR, &sourceRect);
 			}

commit 5c372d80bf775db1c7d4cf85257200754c616eb6

fix CFrame::getCurrentMouseLocation
The use of getTransform ().transform (where) was definitely incorrect;
when using a scaled UI it would return mouse locations outside of the
frame's rect as returned by getSize() or getViewSize()

Seemingly broken by commit 485827ff89af7664f192940f56b619fa46f5c91e

Removing the transform fixes an issue where VSTGUIEditor::onWheel would
send to the wrong view.

There could also be an argument for applying the inverse transform, I'm
not sure what fits best into the vstgui design. But given that
frame->getSize / getViewSize returns untransformed coordinates, one
would expect the same of getCurrentMouseLocation.

The use of getCurrentMouseLocation in GenericOptionMenu::popup looks
suspicious to me; but I have not been able to debug it to determine if
it needs updating.

vstgui/lib/cframe.cpp
@@ -1112,7 +1112,6 @@ bool CFrame::getCurrentMouseLocation (CPoint &where) const
 	{
 		if (pImpl->platformFrame->getCurrentMousePosition (where))
 		{
-			getTransform().transform (where);
 			return true;
 		}
 	}