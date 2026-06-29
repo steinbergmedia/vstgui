8567171b8c0808186846d7260d0f9b84f3297c7f
Will Hurley <will@hurleybirdjr.com>
2026-06-29 18:08:54 +0100

    Refactor arc draw to consider scale factor
    Shoutout @Distb for the original fix for drawEllipse<3
    
    https://github.com/steinbergmedia/vstgui/issues/333

--- vstgui/lib/platform/linux/cairographicscontext.cpp
+++ vstgui/lib/platform/linux/cairographicscontext.cpp
@@ -468,9 +468,12 @@ bool CairoGraphicsDeviceContext::drawArc (CRect rect, double startAngle1, double
 {
 	impl->doInContext ([&] () {
 		CPoint center = rect.getCenter ();
+		cairo_matrix_t save_matrix;
+		cairo_get_matrix( impl->context, & save_matrix );
 		cairo_translate (impl->context, center.x, center.y);
-		cairo_scale (impl->context, 2.0 / rect.getWidth (), 2.0 / rect.getHeight ());
+		cairo_scale (impl->context, 0.5 * rect.getWidth(), 0.5 * rect.getHeight());
 		cairo_arc (impl->context, 0, 0, 1, startAngle1, endAngle2);
+		cairo_set_matrix( impl->context, & save_matrix );
 		impl->draw (drawStyle);
 	});
 	return true;
@@ -479,14 +482,17 @@ bool CairoGraphicsDeviceContext::drawArc (CRect rect, double startAngle1, double
 //------------------------------------------------------------------------
 bool CairoGraphicsDeviceContext::drawEllipse (CRect rect, PlatformGraphicsDrawStyle drawStyle) const
 {
-	impl->doInContext ([&] () {
-		CPoint center = rect.getCenter ();
-		cairo_translate (impl->context, center.x, center.y);
-		cairo_scale (impl->context, 2.0 / rect.getWidth (), 2.0 / rect.getHeight ());
-		cairo_arc (impl->context, 0, 0, 1, 0, 2 * M_PI);
-		impl->draw (drawStyle);
-	});
-	return true;
+    impl->doInContext ([&] () {
+        CPoint center = rect.getCenter ();
+        cairo_matrix_t save_matrix;
+        cairo_get_matrix( impl->context, & save_matrix );
+        cairo_translate (impl->context, center.x, center.y);
+        cairo_scale( impl->context, 0.5 * rect.getWidth(), 0.5 * rect.getHeight() );
+        cairo_arc (impl->context, 0, 0, 1, 0, 2 * M_PI);
+        cairo_set_matrix( impl->context, & save_matrix );
+        impl->draw (drawStyle);
+    });
+    return true;
 }
 
 //------------------------------------------------------------------------
